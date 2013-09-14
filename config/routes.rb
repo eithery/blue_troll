c36d3af6009c18393d7bf8fab16c3bdea5250236
Michael Protsenko <michael.protsenko@gmail.com>
2013-09-14 17:43:39 -0400

    Setup home StaticPagesController action as a root.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 BlueTroll::Application.routes.draw do
-  get "static_pages/home"
   resources :crews
+
+	get 'static_pages/home'
   get 'ticket', to: 'tickets#create'
-  root 'crews#index'
+
+  root 'static_pages#home'
 end
