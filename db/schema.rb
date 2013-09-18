330477d9db7466bf8445703b1f17907834001462
Michael Protsenko <michael.protsenko@gmail.com>
2013-09-18 16:13:32 -0400

    Added unique index to users email attribute.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130918193218) do
+ActiveRecord::Schema.define(version: 20130918201202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,5 +73,7 @@ ActiveRecord::Schema.define(version: 20130918193218) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
