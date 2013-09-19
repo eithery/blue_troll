83164d3cbf91550394e83d504d0e4277b3bf1377
Michael Protsenko <michael.protsenko@gmail.com>
2013-09-19 16:58:00 -0400

    Added email property to participant views.

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -69,6 +69,6 @@ class ParticipantsController < ApplicationController
 
 
   def participant_params
-    params.require(:participant).permit(:last_name, :first_name, :crew, :crew_id, :ticket_code)
+    params.require(:participant).permit(:last_name, :first_name, :crew, :crew_id, :ticket_code, :email)
   end
 end
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -4,11 +4,19 @@ class Participant < ActiveRecord::Base
   validates :ticket_code, presence: true, uniqueness: true, length: { is: 20 }
   validates :crew, presence: true
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX }
+
   belongs_to :crew
 
   def initialize(attributes={})
     super(attributes)
     generate_ticket_code
+  end
+
+
+  def full_name
+    "#{last_name}, #{first_name}"
   end
 
 
