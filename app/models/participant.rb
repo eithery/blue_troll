1db8d3ff6d0145d456d364b76798c37b2d772526
Michael Protsenko <michael.protsenko@gmail.com>
2013-09-19 13:09:34 -0400

    Covered Participant model by unit tests.

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,7 +1,19 @@
 class Participant < ActiveRecord::Base
 	validates :last_name, :first_name, presence: true
   validates :last_name, uniqueness: { :scope => :first_name }
-  validates :ticket_code, presence: true, length: { maximum: 20 }, uniqueness: true
+  validates :ticket_code, presence: true, uniqueness: true, length: { is: 20 }
+  validates :crew, presence: true
 
   belongs_to :crew
+
+  def initialize(attributes={})
+    super(attributes)
+    generate_ticket_code
+  end
+
+
+private
+  def generate_ticket_code
+    self.ticket_code = SecureRandom.hex[0, 20] if self.ticket_code.blank?
+  end
 end
---
+++ spec/models/participant_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe Participant do
+  before do
+    hvost = Crew.new(name: 'Volchij Hvost', native_name: 'Волчий Хвост')
+    @participant = Participant.new(last_name: 'Romanova', first_name: 'Maria', crew: hvost)
+  end
+  subject { @participant }
+
+  it { should respond_to(:last_name) }
+  it { should respond_to(:first_name) }
+  it { should respond_to(:crew) }
+  it { should respond_to(:ticket_code) }
+
+  it { should be_valid }
+
+
+  context "when last name is not present" do
+    before { @participant.last_name = "  " }
+    it { should_not be_valid }
+  end
+
+
+  context "when first name is not present" do
+    before { @participant.first_name = "  " }
+    it { should_not be_valid }
+  end
+
+
+  context "when ticket code is not present" do
+    before { @participant.ticket_code = "  " }
+    it { should_not be_valid }
+  end
+
+
+  context "when crew is not set" do
+    before { @participant.crew = nil }
+    it { should_not be_valid }
+  end
+
+
+  describe "ticket code" do
+    it "length should always be 20 symbols" do
+      @participant.ticket_code.length.should == 20
+    end
+  end
+end
