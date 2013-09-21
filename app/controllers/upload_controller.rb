aef277c3fd031fdb6a8f82d897d6fe8720d0f577
Michael Protsenko <michael.protsenko@gmail.com>
2013-09-21 10:43:44 -0400

    Implemented file upload functionality.

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -1,4 +1,11 @@
 class UploadController < ApplicationController
   def select
   end
+
+  def file_upload
+  	tmp = params[:file_upload][:selected_file].tempfile
+		file = File.join("data", params[:file_upload][:selected_file].original_filename)
+		FileUtils.cp tmp.path, file
+		render 'select'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 BlueTroll::Application.routes.draw do
-  get "upload/select"
   get "users/new"
+
   resources :crews
   resources :participants
 
@@ -8,6 +8,10 @@ BlueTroll::Application.routes.draw do
   get 'ticket', to: 'tickets#create'
   get 'crew_tickets', to: 'tickets#generate_crew_tickets'
   get 'participant_ticket', to: 'tickets#generate_participant_ticket'
+
+  # Administration tasks.
+  get 'upload', to: 'upload#select'
+  post 'upload', to: 'upload#file_upload'
 
   # Root home page.
   root 'static_pages#home'
