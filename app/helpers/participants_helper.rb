fd0c56000a8f06354154e23c696d37bbc8b72fa4
Michael Protsenko <michael.protsenko@gmail.com>
2013-09-24 18:07:40 -0400

    Improved UI for participant and crew lists. Added images.

--- app/helpers/participants_helper.rb
+++ app/helpers/participants_helper.rb
@@ -13,4 +13,14 @@ module ParticipantsHelper
   def form_header
     "#{@participant.new_record? ? "New" : "Edit"} Participant"
   end
+
+
+  def participant_image(participant)
+    image_tag("baby.png", class: "img-small") if participant.category == :baby
+  end
+
+
+  def participant_here_image(participant)
+    image_tag participant.registered_at.blank? ? 'bulb_off.png' : 'bulb_on.png'
+  end
 end
