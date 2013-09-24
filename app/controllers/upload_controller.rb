0717a0f7250e7303fc1197eec8eee675599a7b7a
Michael Protsenko <michael.protsenko@gmail.com>
2013-09-24 13:40:34 -0400

    Implemented participants dump upload functionality from the external source.

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -7,13 +7,24 @@ class UploadController < ApplicationController
 
   def upload_file
 		crews = {}
-		Crew.all.each { |crew| crews[crew.name] = crew }
+		Crew.all.each { |c| crews[c.name] = c }
   	tmp = params[:file_upload][:selected_file].tempfile
 
 		CSV.foreach(tmp.path) do |row|
-			participant = Participant.new(last_name: row[1].strip, first_name: row[2].strip, crew: crews[row[0].strip],
-				child: age_category(row[3]))
-			participant.import_id = row[4].to_i unless row[4].blank?
+      crew_name = row[0].strip
+      last_name = row[1].strip.capitalize
+      first_name = row[2].strip.capitalize
+      category = age_category(row[3])
+      import_id = row[4]
+      reservation_number = row[5]
+
+      crew = crews[crew_name]
+      crew = Crew.find(find_crew_id(crew_name)) if crew.nil?
+
+      participant = find_participant(crew, last_name, first_name)
+      participant = Participant.new(last_name: last_name, first_name: first_name, crew: crew, child: category) if participant.nil?
+      participant.import_id = import_id.blank? || import_id =~ /NULL/i ? nil : import_id.to_i
+      participant.reservation_number = reservation_number
 			participant.save!
 		end
 
@@ -28,4 +39,40 @@ class UploadController < ApplicationController
  		return 2 if age.to_i == 0
  		return 0
  	end
+
+
+  def find_participant(crew, last_name, first_name)
+    Participant.find(:all, conditions: ["crew_id = ? and lower(last_name) = ? and lower(first_name) = ?",
+      crew.id, last_name.downcase, first_name.downcase]).first
+  end
+
+
+  def find_crew_id(crew_name)
+    crews_assoc = {
+      /Звон/ => 1,
+      /Беспризорники/ => 2,
+      /Барабанщики/ => 3,
+      /Дубки/ => 4,
+      /Елки/ => 5,
+      /Эль-Тор/ => 6,
+      /ГТО/ => 7,
+      /Голос/ => 10,
+      /Казаки/ => 11,
+      /Берег/ => 12,
+      /Мандамус/ => 13,
+      /Свои Люди/ => 14,
+      /севера/ => 15,
+      /Палата/ => 16,
+      /Пегас/ => 17,
+      /Разлив/ => 18,
+      /Ослики/ => 20,
+      /TRЯM/ => 21,
+      /Хвост/ => 22,
+      /Ворона/ => 23
+    }
+
+    crews_assoc.each do |key, value|
+      return value if crew_name =~ key
+    end
+  end
 end
