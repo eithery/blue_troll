b1e963398562b39100fef9cce443cf6c9d292caa
Michael Protsenko <michael.protsenko@gmail.com>
2013-09-24 17:23:02 -0400

    Improved look and style of crew and participant lists.

--- app/models/crew.rb
+++ app/models/crew.rb
@@ -10,4 +10,24 @@ class Crew < ActiveRecord::Base
   def to_file_name
     name.gsub(/\s/, '_').downcase
   end
+
+
+  def count
+    participants.to_a.count
+  end
+
+
+  def adults
+    participants.to_a.count { |p| p.category == :adult }
+  end
+
+
+  def children
+    participants.to_a.count { |p| p.category == :child }
+  end
+
+
+  def babies
+    participants.to_a.count { |p| p.category == :baby }
+  end
 end
