51b468740e75420f49bb48181b64056e08d25dd3
Michael Protsenko <michael.protsenko@gmail.com>
2013-09-26 13:54:06 -0400

    Implemented the happy path of new user creation.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to Blue Troll application!"
+      redirect_to @user
     else
       render 'new'
     end
@@ -18,6 +20,6 @@ class UsersController < ApplicationController
 
 private
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
