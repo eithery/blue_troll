8d11c44ee4937bdfaeb03f1bed686a9c88020ef9
Michael Protsenko <michael.protsenko@gmail.com>
2014-03-02 18:39:49 -0500

    Implemented error messages during the new user account registration.

--- app/helpers/user_accounts_helper.rb
+++ app/helpers/user_accounts_helper.rb
@@ -2,6 +2,6 @@ module UserAccountsHelper
   def gravatar_for(user_account)
     gravatar_id = Digest::MD5::hexdigest(user_account.email.downcase)
     gravatar_url = "http://www.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user_account.name, class: "gravatar")
+    image_tag(gravatar_url, alt: user_account.email, class: "gravatar")
   end
 end
