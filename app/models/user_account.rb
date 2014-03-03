6a9ea6cbffcf7c07d927471620acd71143f49f14
Michael Protsenko <michael.protsenko@gmail.com>
2014-03-02 22:55:45 -0500

    Optimized validation for UserAccount model.

--- app/models/user_account.rb
+++ app/models/user_account.rb
@@ -2,9 +2,10 @@ class UserAccount < ActiveRecord::Base
   has_secure_password
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, confirmation: true, uniqueness: { case_sensitive: false }
-  validates :email_confirmation, presence: true
-  validates :password, length: { minimum: 6 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :email, format: { with: VALID_EMAIL_REGEX }, allow_blank: true
+  validates :email, confirmation: true
+  validates :password, length: { minimum: 6 }, allow_blank: true
 
   before_save { self.email.downcase! }
   before_save :create_remember_token
