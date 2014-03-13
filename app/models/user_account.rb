37da255c994fbecff9e4e1e6f292a1b72d4fdd58
Michael Protsenko <michael.protsenko@gmail.com>
2014-03-12 22:51:56 -0400

    Created the set of unit tests for UserAccount model.

--- app/models/user_account.rb
+++ app/models/user_account.rb
@@ -1,13 +1,21 @@
 class UserAccount < ActiveRecord::Base
   has_secure_password
+  has_many :participants
+
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :login, presence: true, length: { minimum: 6 }, uniqueness: { case_sensitive: false }
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :email, format: { with: VALID_EMAIL_REGEX }, allow_blank: true
   validates :email, confirmation: true
+  validates :email_confirmation, presence: true
   validates :password, length: { minimum: 6 }, allow_blank: true
 
-  before_save { self.email.downcase! }
+
+  before_save do
+    self.login.downcase!
+    self.email.downcase!
+  end
   before_save :create_remember_token
 
 
--- spec/models/user_account_spec.rb
+++ spec/models/user_account_spec.rb
@@ -1,98 +1,118 @@
 require 'spec_helper'
 
 describe UserAccount do
-  before { @user_account = UserAccount.new(email: 'jsmith@gmail.com', email_confirmation: 'jsmith@gmail.com',
+  before { @user_account = UserAccount.new(login: 'jsmith', email: 'jsmith@gmail.com', email_confirmation: 'jsmith@gmail.com',
     password: 'secret', password_confirmation: 'secret') }
   subject { @user_account }
 
+  it { should respond_to :login }
   it { should respond_to :email }
   it { should respond_to :email_confirmation }
   it { should respond_to :password_digest }
   it { should respond_to :password }
   it { should respond_to :password_confirmation }
   it { should respond_to :active? }
+  it { should respond_to :activated_at }
   it { should respond_to :authenticate }
+  it { should respond_to :participants }
   it { should be_valid }
   it { should_not be_active }
 
 
-  context "when email is not entered" do
-    before { @user_account.email = "  " }
-    it { should_not be_valid }
+  describe "when login" do
+    context "is not entered" do
+      before { @user_account.login = "  " }
+      it { should_not be_valid }
+    end
+
+    context "is too short" do
+      before { @user_account.login = "a" * 5 }
+      it { should_not be_valid }
+    end
+
+    context "is duplicated" do
+      before do
+        existing_user_account = @user_account.dup
+        existing_user_account.login = @user_account.login.upcase
+        existing_user_account.save
+      end
+      it { should_not be_valid }
+    end
   end
 
 
-  context "when email confirmation is not entered" do
-    before { @user_account.email_confirmation = " " }
-    it { should_not be_valid }
-  end
+  describe "when email" do
+    context "is not entered" do
+      before { @user_account.email = "  " }
+      it { should_not be_valid }
+    end
 
+    context "confirmation is not entered" do
+      before { @user_account.email_confirmation = " " }
+      it { should_not be_valid }
+    end
 
-  context "when email does not match confirmation" do
-    before { @user_account.email_confirmation = 'mismatch' }
-    it { should_not be_valid }
-  end
+    context "does not match confirmation" do
+      before { @user_account.email_confirmation = 'mismatch' }
+      it { should_not be_valid }
+    end
 
+    context "confirmation is nil" do
+      before { @user_account.email_confirmation = nil }
+      it { should_not be_valid }
+    end
 
-  context "when email confirmation is nil" do
-    before { @user_account.email_confirmation = nil }
-    it { should_not be_valid }
-  end
-
-
-  context "when email format is invalid" do
-    it "should be invalid" do
-      emails = %w[user@foo,com user_at_foo.org example.user@foo.foo@bar_baz.com foo_bar+baz.com]
-      emails.each do |invalid_email|
-        @user_account.email = @user_account.email_confirmation = invalid_email
-        @user_account.should_not be_valid
+    context "format is invalid" do
+      it "should be invalid" do
+        emails = %w[user@foo,com user_at_foo.org example.user@foo.foo@bar_baz.com foo_bar+baz.com]
+        emails.each do |invalid_email|
+          @user_account.email = @user_account.email_confirmation = invalid_email
+          @user_account.should_not be_valid
+        end
       end
     end
-  end
 
-
-  context "when email format is valid" do
-    it "should be valid" do
-      emails = %w[user@foo.COM A_US-ER@f.b.org first.last@foo.jp a+b@baz.cn]
-      emails.each do |valid_email|
-        @user_account.email = @user_account.email_confirmation = valid_email
-        @user_account.should be_valid
+    context "format is valid" do
+      it "should be valid" do
+        emails = %w[user@foo.COM A_US-ER@f.b.org first.last@foo.jp a+b@baz.cn]
+        emails.each do |valid_email|
+          @user_account.email = @user_account.email_confirmation = valid_email
+          @user_account.should be_valid
+        end
       end
     end
-  end
 
-
-  context "when email address is duplicated" do
-    before do
-      existing_user_account = @user_account.dup
-      existing_user_account.email = existing_user_account.email_confirmation = @user_account.email.upcase
-      existing_user_account.save
+    context "is duplicated" do
+      before do
+        existing_user_account = @user_account.dup
+        existing_user_account.email = existing_user_account.email_confirmation = @user_account.email.upcase
+        existing_user_account.save
+      end
+      it { should_not be_valid }
     end
-    it { should_not be_valid }
   end
 
 
-  context "when password is not present" do
-    before { @user_account.password = @user_account.password_confirmation = "  " }
-    it { should_not be_valid }
-  end
+  describe "when password" do
+    context "is not present" do
+      before { @user_account.password = @user_account.password_confirmation = "  " }
+      it { should_not be_valid }
+    end
 
+    context "does not match confirmation" do
+      before { @user_account.password_confirmation = 'mismatch'}
+      it { should_not be_valid }
+    end
 
-  context "when password does not match confirmation" do
-    before { @user_account.password_confirmation = 'mismatch'}
-    it { should_not be_valid }
-  end
+    context "confirmation is nil" do
+      before { @user_account.password_confirmation = nil }
+      it { should_not be_valid }
+    end
 
-
-  context "when password confirmation is nil" do
-    before { @user_account.password_confirmation = nil }
-    it { should_not be_valid }
-  end
-
-
-  context "with a password that's too short" do
-    before { @user_account.password = @user_account.password_confirmation = "a" * 5 }
-    it { should_not be_valid }
+    context "is too short" do
+      before { @user_account.password = @user_account.password_confirmation = "a" * 5 }
+      it { should_not be_valid }
+    end
   end
 
 
