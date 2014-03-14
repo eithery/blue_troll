47438a29b52ce3622655568bede6125b17ea3c97
Michael Protsenko <michael.protsenko@gmail.com>
2014-03-14 13:59:30 -0400

    Added some new data columns to participants data table.

--- db/migrate/20130907231620_create_participants.rb
+++ db/migrate/20130907231620_create_participants.rb
@@ -10,7 +10,7 @@ class CreateParticipants < ActiveRecord::Migration
       t.string :first_name, null: false
       t.string :middle_name
       t.integer :gender
-      t.integer :child, null: false, default: 0
+      t.integer :age_category, null: false, default: 0
       t.date :born_on
 
       # Contacts and address.
@@ -22,6 +22,7 @@ class CreateParticipants < ActiveRecord::Migration
       t.string :city
       t.string :state, limit: 2
       t.string :zip, limit: 5
+      t.string :country
 
       # Ticket info.
       t.string :ticket_code
@@ -31,6 +32,7 @@ class CreateParticipants < ActiveRecord::Migration
       t.string :approved_by
       t.datetime :payment_sent_at
       t.string :payment_sent_by
+      t.string :payment_notes
       t.datetime :payment_confirmed_at
       t.string :payment_confirmed_by
       t.datetime :registered_at
--- db/schema.rb
+++ db/schema.rb
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 20130918193218) do
     t.string   "first_name",                                     null: false
     t.string   "middle_name"
     t.integer  "gender"
-    t.integer  "child",                          default: 0,     null: false
+    t.integer  "age_category",                   default: 0,     null: false
     t.date     "born_on"
     t.string   "home_phone"
     t.string   "cell_phone"
@@ -58,11 +58,13 @@ ActiveRecord::Schema.define(version: 20130918193218) do
     t.string   "city"
     t.string   "state",                limit: 2
     t.string   "zip",                  limit: 5
+    t.string   "country"
     t.string   "ticket_code"
     t.datetime "approved_at"
     t.string   "approved_by"
     t.datetime "payment_sent_at"
     t.string   "payment_sent_by"
+    t.string   "payment_notes"
     t.datetime "payment_confirmed_at"
     t.string   "payment_confirmed_by"
     t.datetime "registered_at"
