d611ce282dd5e884f1a71f32f7ab1559b91a6697
Michael Protsenko <michael.protsenko@gmail.com>
2014-03-14 14:00:35 -0400

    Created more unit tests for Participant model.

--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -12,7 +12,30 @@ describe Participant do
   it { should respond_to :last_name }
   it { should respond_to :first_name }
   it { should respond_to :middle_name }
+  it { should respond_to :gender }
+  it { should respond_to :age_category }
+  it { should respond_to :born_on }
+  it { should respond_to :home_phone }
+  it { should respond_to :cell_phone }
+  it { should respond_to :email }
+  it { should respond_to :address_line_1 }
+  it { should respond_to :address_line_2 }
+  it { should respond_to :city }
+  it { should respond_to :state }
+  it { should respond_to :zip }
+  it { should respond_to :country }
   it { should respond_to :ticket_code }
+  it { should respond_to :approved_at }
+  it { should respond_to :approved_by }
+  it { should respond_to :payment_sent_at }
+  it { should respond_to :payment_sent_by }
+  it { should respond_to :payment_notes }
+  it { should respond_to :payment_confirmed_at }
+  it { should respond_to :payment_confirmed_by }
+  it { should respond_to :registered_at }
+  it { should respond_to :registered_by }
+  it { should respond_to :flagged? }
+  it { should respond_to :notes }
   it { should respond_to :created_at }
   it { should respond_to :updated_at }
 
