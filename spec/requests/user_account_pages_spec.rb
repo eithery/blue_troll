4c9f18f64bbfded2011c19b7f51b39b95ebd56e3
Michael Protsenko <michael.protsenko@gmail.com>
2014-03-20 17:34:26 -0400

    Created the set of specs for happy and fail pathes of create the new user account use case.

--- spec/requests/user_account_pages_spec.rb
+++ spec/requests/user_account_pages_spec.rb
@@ -18,4 +18,42 @@ describe "User account pages" do
     it_behaves_like "Page with header"
     it_behaves_like "Page with footer"
   end
+
+
+  describe "signup" do
+    before { visit signup_path }
+    let(:submit) { "Create my account" }
+
+    context "with invalid information" do
+      it "should not create a user account" do
+        expect { click_button submit }.not_to change(UserAccount, :count)
+      end
+    end
+
+
+    context "with valid information" do
+      before do
+        fill_in 'Login', with: 'maryikaro'
+        fill_in 'Email', with: 'maryika@gmail.com'
+        fill_in 'Email Confirmation', with: 'maryika@gmail.com'
+        fill_in 'Password', with: 'foobar123'
+        fill_in 'Password Confirmation', with: 'foobar123'
+      end
+
+      it "should create a user account" do
+        expect { click_button submit }.to change(UserAccount, :count).by(1)
+      end
+    end
+  end
+
+
+  describe "profile page" do
+    let(:user_account) { FactoryGirl.create(:user_account) }
+    before { visit user_account_path(user_account) }
+
+    it { should have_title "Blue Troll | #{user_account.name} profile" }
+
+    it_behaves_like "Page with header"
+    it_behaves_like "Page with footer"
+  end
 end
