30cc2c80c8b62a1878b2a4d53a96af9ed9620c5d
Michael Protsenko <michael.protsenko@gmail.com>
2014-03-31 00:45:14 -0400

    Corrected registration mailer and template.

--- app/mailers/registration_notifier.rb
+++ app/mailers/registration_notifier.rb
@@ -1,14 +1,8 @@
 class RegistrationNotifier < ActionMailer::Base
-  default from: "bluetrolley2014@gmail.com"
+  default from: "Blue Trolley <bluetrolley2014@gmail.com>"
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.registration_notifier.registered.subject
-  #
+
   def registered
-    @greeting = "Hi from Blue Trolley!"
-
-    mail to: "michael.protsenko@mandosys.com"
+    mail to: "michael.protsenko@gmail.com", subject: "New Blue Trolley account created"
   end
 end
