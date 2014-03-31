7a2daa6e9107af277412a6a63664994a9227a99e
Michael Protsenko <michael.protsenko@gmail.com>
2014-03-31 18:11:41 -0400

    Restored SessionHelper class.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,22 +1,21 @@
 module SessionsHelper
-  def sign_in(user)
-    cookies.permanent[:remember_token] = user.remember_token
-    self.current_user = user
+  def sign_in(user_account)
+    cookies.permanent[:remember_token] = user_account.remember_token
+    self.current_user = user_account
   end
 
 
-  def current_user=(user)
-    @current_user = user
+  def current_user=(user_account)
+    @current_user = user_account
   end
 
 
   def current_user
-    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+    @current_user ||= UserAccount.find_by_remember_token(cookies[:remember_token])
   end
 
 
   def signed_in?
-#    !current_user.nil?
-    false
+    !current_user.nil?
   end
 end
