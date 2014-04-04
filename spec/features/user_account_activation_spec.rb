3569bbf3962535704719c4cd1a38bca44a468e5c
Michael Protsenko <michael.protsenko@gmail.com>
2014-04-04 17:23:43 -0400

    Created spec placeholders for 'user account activation' feature.

---
+++ spec/features/user_account_activation_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe "User account activation" do
+  describe "with correct activation code" do
+  end
+
+
+  describe "with incorrect activation code" do
+  end
+
+
+  describe "with correct activation link" do
+  end
+
+
+  describe "with incorrect activation link" do
+  end
+end
