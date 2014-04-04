6180a80cc44858c9d64d7e681ae498231517f397
Michael Protsenko <michael.protsenko@gmail.com>
2014-04-04 15:51:10 -0400

    Completed new account registration feature (happy path).

--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -1,25 +1,68 @@
 require 'spec_helper'
 
-describe "New user registration" do
+describe "User account registration:" do
+  let(:user_account) { FactoryGirl.build(:user_account) }
+  let(:create_account) { 'Create my account' }
   subject { page }
   before do
     visit root_path
     click_link 'Register now!'
   end
 
-  it { should have_title('New User Account') }
+
+  describe "user is navigated to new user account page" do
+    it { should have_title('New User Account') }
+  end
 
 
-  context "with valid entered information (happy path)" do
-    before do
-      fill_in 'Login', with: 'donreba'
-      fill_in 'Password', with: 'secret'
-      fill_in 'Password Confirmation', with: 'secret'
-      fill_in 'Email', with: 'donreba@gmail.com'
-      fill_in 'Email Confirmation', with: 'donreba@gmail.com'
-      click_button 'Create my account'
+  describe "when user is not registered yet" do
+    describe "and enters and submit all valid information (happy path)" do
+      before do
+        fill_in 'Login', with: user_account.login
+        fill_in 'Password', with: user_account.password
+        fill_in 'Password Confirmation', with: user_account.password
+        fill_in 'Email', with: user_account.email
+        fill_in 'Email Confirmation', with: user_account.email
+      end
+
+
+      specify "new user account should be created" do
+        expect { click_button create_account }.to change{ UserAccount.count }.by(1)
+      end
+
+
+      describe "user is navigated to account activation page" do
+        before { click_button create_account }
+
+        it { should have_title('Account Activation') }
+        it { should have_content("Hello #{user_account.login}, Welcome to Blue Trolley club!") }
+        it { should have_content("Within few minutes, you will receive an email " +
+          "with your activation link and activation code.") }
+        it { should have_content("The email is sent to the following address: #{user_account.email}") }
+        it { should have_content("In order to activate your account enter the code or " +
+          "click on the link in your email.") }
+        it { should have_selector('.alert-success', text: "New user account for #{user_account.login} is created.") }
+      end
+
+
+      describe "user receives email" do
+        let(:mail) { RegistrationNotifier.deliveries.first }
+        subject { mail }
+        before { click_button create_account }
+
+        specify { mail.to.should include(user_account.email) }
+        specify { mail.subject.should == "Blue Trolley club account activation" }
+
+        describe "body" do
+          subject { mail.body.encoded }
+          it { should =~ /https:\/\/bluetrolley2014\./ }
+          it { should =~ /Your account activation code is: [0-9]{10}/ }
+        end
+      end
     end
+  end
 
-    it { should have_title('Account Activation') }
+
+  describe "when user is already registered" do
   end
 end
