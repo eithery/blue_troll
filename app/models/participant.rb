836892f0515cf59f1ba2e138c265c3ad3252277e
Michael Protsenko <michael.protsenko@gmail.com>
2014-04-07 23:15:06 -0400

    Improved user profile look and feel.

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -26,6 +26,11 @@ class Participant < ActiveRecord::Base
   end
 
 
+  def email
+    self[:email].empty? ? user_account.email : self[:email]
+  end
+
+
   def Participant.find_by_ticket(ticket_code)
     Participant.all.each do |participant|
       return participant if participant.ticket_code.to_i(16).to_s == ticket_code.strip
