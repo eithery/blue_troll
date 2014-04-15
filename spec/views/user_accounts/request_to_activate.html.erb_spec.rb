ee699a5f4fa00b907c30743b0b826d0f47332d18
Michael Protsenko <michael.protsenko@gmail.com>
2014-04-15 17:05:10 -0400

    Created specs for user_accounts/request_to_activate view.

--- spec/views/user_accounts/request_to_activate.html.erb_spec.rb
+++ spec/views/user_accounts/request_to_activate.html.erb_spec.rb
@@ -2,17 +2,19 @@ require 'spec_helper'
 
 describe "user_accounts/request_to_activate.html.erb" do
   subject { rendered }
-  let(:user_login) { 'johnsmith' }
-  let(:user_email) { 'johnsmith@gmail.com' }
+  let(:user) { stub_model(UserAccount, login: 'johnsmith', name: 'johnsmith', email: 'johnsmith@gmail.com') }
   before do
-    assign(:user, stub_model(UserAccount, name: user_login, email: user_email))
+    assign(:user, user)
     render
   end
 
-  it { should have_content("Hello #{user_login},") }
+  it { should have_content("Hello #{user.login}, welcome to Blue Trolley club!") }
   it { should have_content('Your registration has been successful.') }
-  it { should have_content("The email is sent to the following address: #{user_email}") }
+  it { should have_content("The email is sent to the following address: #{user.email}") }
+  it { should have_selector("form[action='#{activate_path}']") }
+
   it { should have_field('Activation Code:') }
   it { should have_button('Activate my account') }
-  it { should have_link('here') }
+  it { should have_content('No email received?') }
+  it { should have_link('here', href: '#') }
 end
