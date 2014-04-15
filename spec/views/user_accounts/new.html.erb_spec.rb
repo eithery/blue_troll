08ad42bda64dad75f43d8952e4d8592d325e7cd7
Michael Protsenko <michael.protsenko@gmail.com>
2014-04-15 13:29:29 -0400

    Created view spec for user_accounts/new view.

---
+++ spec/views/user_accounts/new.html.erb_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe "user_accounts/new.html.erb" do
+  subject { rendered }
+  let(:user) { stub_model(UserAccount).as_new_record }
+  before do
+    assign(:user, user)
+    render
+  end
+
+  it { should have_selector("form[action='#{user_accounts_path}']") }
+
+  it { should have_field('Login', text: '') }
+  it { should have_field('Password', text: '') }
+  it { should have_field('Password Confirmation', text: '') }
+  it { should have_field('Email', text: '') }
+  it { should have_field('Email Confirmation', text: '') }
+
+  it { should have_button('Create my account') }
+end
