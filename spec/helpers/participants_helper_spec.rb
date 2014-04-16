06e66b1e94485ae0aaa07303a2e642985d79f0f4
Michael Protsenko <michael.protsenko@gmail.com>
2014-04-16 13:20:21 -0400

    Implemented specs for ParticipantsHelper class.

---
+++ spec/helpers/participants_helper_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe ParticipantsHelper do
+  describe "#all_crews" do
+    it "returns all crews" do
+      FactoryGirl.create(:crew)
+      FactoryGirl.create(:spies)
+      FactoryGirl.create(:fix_crew)
+
+      all_crews.should have(3).items
+    end
+
+
+    it "orders crews by name" do
+      Crew.should_receive(:order).with(:name)
+      all_crews
+    end
+  end
+
+
+  describe "#form_header" do
+    subject { form_header }
+
+    context "for new participant" do
+      before { @participant = mock_model(Participant).as_new_record }
+      it { should == "New Participant" }
+    end
+
+
+    context "for existing participant" do
+      before { @participant = mock_model(Participant) }
+      it { should == "Edit Participant" }
+    end
+  end
+end
