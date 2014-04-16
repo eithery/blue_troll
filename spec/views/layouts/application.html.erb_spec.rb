d32abb1bf56789f3b1c9e665098242fc265fa19a
Michael Protsenko <michael.protsenko@gmail.com>
2014-04-16 10:36:06 -0400

    Created view specs for layouts/application main view.

---
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe "layouts/application.html.erb" do
+  subject { rendered }
+  let(:warning) { "Some test warning message" }
+  before { render }
+
+  it { should have_title('Blue Troll') }
+  it { should have_selector('div.container', minimum: 2) }
+
+
+  context "without flash messages" do
+    it { should_not have_selector('div.alert') }
+    it { should_not have_selector('button') }
+  end
+
+
+  context "with flash" do
+    before do
+      flash[:warning] = warning
+      render
+    end
+
+    it { should have_selector('div.alert') }
+    it { should have_content(warning) }
+    it { should have_selector('button.close') }
+  end
+end
