34a25f514c50e581508c8bea45384db8ebdc0946
Michael Protsenko <michael.protsenko@gmail.com>
2014-04-17 16:07:03 -0400

    Used activation token in email link generation.

--- spec/views/registration_notifier/registered.text.erb_spec.rb
+++ spec/views/registration_notifier/registered.text.erb_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe "registration_notifier/registered.text.erb" do
   subject { rendered }
   let(:activation_code) { '123456789' }
-  let(:user) { stub_model(UserAccount, activation_code: activation_code) }
+  let(:activation_token) { SecureRandom.uuid }
+  let(:user) { stub_model(UserAccount, activation_code: activation_code, activation_token: activation_token) }
   before do
     assign(:user, user)
     render
@@ -11,11 +12,16 @@ describe "registration_notifier/registered.text.erb" do
 
   it { should have_content('Thank you for registering in Blue Trolley club') }
   it { should have_content('Please click link below to activate your account.') }
-  it { should have_content("https://bluetrolley2014.heroku.com/activation?activation_id=#{activation_code}") }
+  it { should have_content("https://bluetrolley2014.herokuapp.com/activation?activation_token=#{activation_token}") }
   it { should have_content("Your account activation code is: #{activation_code}") }
 
   specify do
-    user.should_receive(:activation_code).twice
+    user.should_receive(:activation_code)
+    render
+  end
+
+  specify do
+    user.should_receive(:activation_token)
     render
   end
 end
