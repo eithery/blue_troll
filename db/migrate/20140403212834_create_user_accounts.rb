c7aeb46a658d6d9c1fa8748afee1cfa8653f30e0
Michael Protsenko <michael.protsenko@gmail.com>
2014-04-17 17:45:36 -0400

    Added all crew leads to migration.

--- db/migrate/20140403212834_create_user_accounts.rb
+++ db/migrate/20140403212834_create_user_accounts.rb
@@ -25,14 +25,63 @@ class CreateUserAccounts < ActiveRecord::Migration
     add_index :user_accounts, :remember_token
 
 
-    UserAccount.create!(login: 'sa',
-      email: 'michael.protsenko@gmail.com', email_confirmation: 'michael.protsenko@gmail.com',
-      password: 'secret', password_confirmation: 'secret', active: true, dev: true, gatekeeper: true,
-      financier: true, admin: true)
+    devs = [{ login: 'sa', email: 'michael.protsenko@gmail.com', email_confirmation: 'michael.protsenko@gmail.com',
+      password: 'secret', password_confirmation: 'secret', active: true, dev: true, gatekeeper: true, financier: true,
+      admin: true }]
+
+    crew_leads = [
+      { login: 'alexshayk', email: 'alexshayk@yahoo.com', password: generate, admin: true, financier: true },
+      { login: 'ashaykevich', email: 'alexshayk@gmail.com', password: generate },
+      { login: 'ypogorelsky', email: 'ypogorelsky@hotmail.com', password: generate },
+      { login: 'izaverukha', email: 'ilyazaver@gmail.com', password: generate },
+      { login: 'rhamon', email: 'romanhamon3@yahoo.com', password: generate },
+      { login: 'bserebrenik', email: 'bserebrenik@gmail.com', password: generate },
+      { login: 'ituchina', email: 'iratuchin@netscape.net', password: generate },
+      { login: 'apolyakov', email: 'alekspolyakov@yahoo.com', password: generate },
+      { login: 'abelyaev', email: 'alexander@belyayev.com', password: generate },
+      { login: 'idricker', email: 'ilya.dricker@gmail.com', password: generate },
+      { login: 'mbrandis', email: 'leto4you@yahoo.com', password: generate },
+      { login: 'igudgartc', email: 'morpeh92@yahoo.com', password: generate },
+      { login: 'makhmetov', email: 'marina.mashka@gmail.com', password: generate },
+      { login: 'agrinberg', email: 'annagrin@yahoo.com', password: generate },
+      { login: 'vkazakov', email: 'victorkazak@gmail.com', password: generate },
+      { login: 'dtalalaev', email: 'fatcatd@gmail.com', password: generate },
+      { login: 'vkosulin', email: 'kosulin@yahoo.com', password: generate },
+      { login: 'gpalitsky', email: 'superblin@gmail.com', password: generate },
+      { login: 'veidelstein', email: 'vishinka@yahoo.com', password: generate },
+      { login: 'ylev', email: 'yuri.j.lev@gmail.com', password: generate },
+      { login: 'ryeliseyev', email: 'ryeliseyev@gmail.com', password: generate, financier: true },
+      { login: 'iaks', email: 'irina.akc@gmail.com', password: generate },
+      { login: 'jpliner', email: 'jplinerinteriors@gmail.com', password: generate },
+      { login: 'vkhazak', email: 'vladimirkhazak@yahoo.com', password: generate },
+      { login: 'azaks', email: 'demzak@yahoo.com', password: generate },
+      { login: 'omikaloff', email: 'omikaloff@yahoo.com', password: generate },
+      { login: 'astolov', email: 'andrei_stolov@yahoo.com', password: generate },
+      { login: 'kkarpova', email: 'katekarpova@yahoo.com', password: generate },
+      { login: 'lrapoport', email: 'lvrapoport@gmail.com', password: generate },
+      { login: 'imashkovich', email: 'inna.mashkovich@power.alstom.com', password: generate },
+      { login: 'igrapp', email: 'igrapp@gmail.com', password: generate }
+    ]
+
+    crew_leads.each do |lead|
+      lead[:email_confirmation] = lead[:email]
+      lead[:password_confirmation] = lead[:password]
+      lead[:crew_lead] = true
+      lead[:active] = true
+      lead[:activated_at] = Time.now
+    end
+
+    UserAccount.create!(devs + crew_leads)
   end
 
 
   def down
     drop_table :user_accounts
+  end
+
+
+private
+  def generate
+    SecureRandom.urlsafe_base64
   end
 end
