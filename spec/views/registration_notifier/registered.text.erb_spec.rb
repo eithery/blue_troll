79bb36bd575cbbb9433d813aa050e27a42ddf2c7
Michael Protsenko <michael.protsenko@gmail.com>
2014-04-18 20:50:12 -0400

    Fixed account activation link.

--- spec/views/registration_notifier/registered.text.erb_spec.rb
+++ spec/views/registration_notifier/registered.text.erb_spec.rb
@@ -12,7 +12,7 @@ describe "registration_notifier/registered.text.erb" do
 
   it { should have_content('Thank you for registering in Blue Trolley club') }
   it { should have_content('Please click link below to activate your account.') }
-  it { should have_content("https://bluetrolley2014.herokuapp.com/activation?activation_token=#{activation_token}") }
+  it { should have_content("https://bluetrolley2014.herokuapp.com/activate?activation_token=#{activation_token}") }
   it { should have_content("Your account activation code is: #{activation_code}") }
 
   specify do
