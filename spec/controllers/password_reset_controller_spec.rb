892240e2426e737205cf75bab2bd78fbe2b698d9
Michael Protsenko <michael.protsenko@gmail.com>
2014-04-18 23:31:55 -0400

    Created the set of pending specs for PasswordResetController class.

--- spec/controllers/password_reset_controller_spec.rb
+++ spec/controllers/password_reset_controller_spec.rb
@@ -9,10 +9,23 @@ describe PasswordResetController do
   end
 
 
-  describe "POST create" do
+  describe "POST send_link" do
+    it "finds user by login"
+    it "finds user by email"
+
+    context "when login or email exists" do
+      it "send email with reset password link"
+      it "redirects to change password page"
+    end
+
+
+    context "when login or email cannot be found" do
+      it "displays a flash warning message"
+      it "renders collect info template"
+    end
   end
 
 
-  describe "GET show" do
+  describe "GET reset" do
   end
 end
