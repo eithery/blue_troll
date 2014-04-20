4a27e0396fca47f73dc42b798b6d92dcae2ccf17
Michael Protsenko <michael.protsenko@gmail.com>
2014-04-20 15:16:38 -0400

    Created change password controller and its spec file.

---
+++ app/controllers/change_password_controller.rb
@@ -0,0 +1,10 @@
+class ChangePasswordController < ApplicationController
+  def edit
+    redirect_to signin_path
+  end
+
+
+  def update
+    redirect_to signin_path
+  end
+end
---
+++ spec/controllers/change_password_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe ChangePasswordController do
+  describe "GET edit" do
+  end
+
+
+  describe "PUT update" do
+  end
+end
