040e3563e1f13851dad23f3e907fa7e403a70ba8
Michael Protsenko <michael.protsenko@gmail.com>
2014-04-20 18:32:17 -0400

    Deleted change password controller and its specs.

---
+++ spec/views/user_accounts/change_password.html.erb_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe "user_accounts/change_password.html.erb" do
+  subject { rendered }
+  before { render }
+
+  it { should have_selector("form[action='#{change_password_path}']") }
+  it { should have_selector("legend", text: "Change your password")}
+  it { should have_field("Email") }
+  it { should have_field("Password") }
+  it { should have_field("Password confirmation") }
+
+  it { should have_button("Change password") }
+  it { should have_link("Cancel", href: signin_path) }
+end
