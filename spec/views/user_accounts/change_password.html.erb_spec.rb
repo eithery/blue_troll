d2575c53560f02088f80f49b8eec00a5e5402ed4
Michael Protsenko <michael.protsenko@gmail.com>
2014-04-20 18:42:09 -0400

    Covered by view specs change password template view.

--- spec/views/user_accounts/change_password.html.erb_spec.rb
+++ spec/views/user_accounts/change_password.html.erb_spec.rb
@@ -2,10 +2,14 @@ require 'spec_helper'
 
 describe "user_accounts/change_password.html.erb" do
   subject { rendered }
-  before { render }
+  before do
+    assign(:user, mock_model(UserAccount))
+    render
+  end
 
   it { should have_selector("form[action='#{change_password_path}']") }
-  it { should have_selector("legend", text: "Change your password")}
+  it { should have_selector("legend", text: "Change your password") }
+  it { should have_selector("input#user_account_email[readonly='readonly']") } 
   it { should have_field("Email") }
   it { should have_field("Password") }
   it { should have_field("Password confirmation") }
