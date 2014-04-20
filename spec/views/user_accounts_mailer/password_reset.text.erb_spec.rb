e5b8ea343397429b594307c622b5c819c2fb6dcc
Michael Protsenko <michael.protsenko@gmail.com>
2014-04-19 23:48:51 -0400

    Implemented view specs for password reset action of UserAccountMailer class.

---
+++ spec/views/user_accounts_mailer/password_reset.text.erb_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe "user_accounts_mailer/password_reset.text.erb" do
+  subject { rendered }
+  let(:reset_token) { SecureRandom.uuid }
+  let(:user) { mock_model(UserAccount, name: 'gwen', reset_password_token: reset_token) }
+  before do
+    assign(:user, user)
+    render
+  end
+
+  it { should have_content("We received a request to reset the password associated with this email address") }
+  it { should have_content("Click the link below to reset your password") }
+  it { should have_content("https://bluetrolley2014.herokuapp.com/pwd_reset?reset_token=#{reset_token}")}
+
+  specify do
+    user.should_receive(:name)
+    render
+  end
+
+  specify do
+    user.should_receive(:reset_password_token)
+    render
+  end
+end
