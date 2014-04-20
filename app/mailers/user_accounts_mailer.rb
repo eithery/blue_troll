aedeb7c990eec79fcaa27b678a69ee3271ba3f88
Michael Protsenko <michael.protsenko@gmail.com>
2014-04-19 21:50:57 -0400

    Implemented specs for UserAccountMailer class. Created empty mail templates.

--- app/mailers/user_accounts_mailer.rb
+++ app/mailers/user_accounts_mailer.rb
@@ -1,20 +1,39 @@
 class UserAccountsMailer < ActionMailer::Base
-  default from: "bluetrolley2014@gmail.com"
-
+  # Sends email when user created the new account in the system.
+  # The email contains the link to activate the user account.
   def registered(user)
-    @user = user
-    mail to: user.email, subject: "Blue Trolley club account activation"
+    mail_to user, "Blue Trolley: club account activation"
   end
 
 
+  # Sends confirmation email when user activated her account.
   def activated(user)
+    mail_to user, "Blue Trolley: account has been activated"
   end
 
 
+  # Sends email contaning the link to reset user's password.
   def password_reset(user)
+    mail_to user, "Blue Trolley: password reset"
   end
 
 
+  # Sends notification email when password is changed.
   def password_changed(user)
+    mail_to user, "Blue Trolley: password has been changed"
+  end
+
+private
+  def club_email
+    "Blue_Trolley <bluetrolley2014@gmail.com>"
+  end
+
+  def admin_email
+    'michael.protsenko@gmail.com'
+  end
+
+  def mail_to(user, subject)
+    @user = user
+    mail to: @user.email, bcc: admin_email, from: club_email, subject: subject
   end
 end
--- spec/mailers/user_accounts_mailer_spec.rb
+++ spec/mailers/user_accounts_mailer_spec.rb
@@ -1,18 +1,47 @@
 require "spec_helper"
 
+shared_examples_for "user accounts mailer" do
+  it "sent to user email address" do
+    mail.to.should include(user.email)
+  end
+
+  it "duplicated to admin address" do
+    mail.bcc.should include('michael.protsenko@gmail.com')
+  end
+
+  it "sent from Blue Trolley club email address" do
+    mail.from.should include('bluetrolley2014@gmail.com')      
+  end
+end
+
+
 describe UserAccountsMailer do
+  let(:user) { mock_model(UserAccount, email: 'gwen@gmail1.com').as_null_object }
+
   describe "#registered" do
-    let(:user) { FactoryGirl.build(:inactive_user) }
     let(:mail) { UserAccountsMailer.registered(user) }
+    specify { mail.subject.should == "Blue Trolley: club account activation" }
+    it_behaves_like "user accounts mailer"
+  end
 
-    it "renders the headers" do
-      mail.subject.should == "Blue Trolley club account activation"
-      mail.to.should include(user.email)
-      mail.from.should include('bluetrolley2014@gmail.com')
-    end
 
-    it "renders the body" do
-      mail.body.encoded.should =~ /Thank you for registering in Blue Trolley club./
-    end
+  describe "#activated" do
+    let(:mail) { UserAccountsMailer.activated(user) }
+    specify { mail.subject.should == "Blue Trolley: account has been activated" }
+    it_behaves_like "user accounts mailer"
+  end
+
+
+  describe "#password_reset" do
+    let(:mail) { UserAccountsMailer.password_reset(user) }
+    specify { mail.subject.should == "Blue Trolley: password reset" }
+    it_behaves_like "user accounts mailer"
+  end
+
+
+  describe "#password_changed" do
+    let(:mail) { UserAccountsMailer.password_changed(user) }
+    specify { mail.subject.should == "Blue Trolley: password has been changed" }
+    it_behaves_like "user accounts mailer"
   end
 end
