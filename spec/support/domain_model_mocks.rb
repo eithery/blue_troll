77db89f84e78849b4c3f31b93c682616c296f9a6
Michael Protsenko <michael.protsenko@gmail.com>
2014-04-21 19:02:32 -0400

    Created DomainModelMocks module providing the set mocks and stubs for domain object models.

---
+++ spec/support/domain_model_mocks.rb
@@ -0,0 +1,41 @@
+module DomainModelMocks
+  def mock_user_account
+    mock_model(UserAccount).as_null_object
+  end
+
+
+  def mock_participant
+    mock_model(Participant, user_account: user_account_mock).as_null_object
+  end
+
+
+  def stub_participant(stubs={})
+    participant = stub_model(Participant, user_account: mock_user_account, crew: mock_crew, last_name: 'Smith',
+      first_name: 'Gwen', age_category: AgeCategory::BABY, age: 3, email: 'gwen@gmail1.com',
+      cell_phone: '347-583-0140', address_line_1: '31 Gadsen Pl Staten Island NY 10314')
+
+    stubs.each { |key, val| participant.stub(key).and_return(val) }
+    participant
+  end
+
+
+  def stub_new_participant(stubs={})
+    participant = stub_model(Participant, user_account: mock_user_account).as_new_record
+    stubs.each { |key, val| participant.stub(key).and_return(val) }
+    participant
+  end
+
+
+  def mock_crew
+    mock_model(Crew, name: 'Guests', native_name: 'Гости Слета').as_null_object
+  end
+
+
+  def mock_crews
+    [
+      mock_model(Crew, name: 'Enemy spies', native_name: 'Вражеские шпионы').as_null_object,
+      mock_model(Crew, name: 'Mr Fix Friends', native_name: 'Друганы Мистера Фикса').as_null_object,
+      mock_crew
+    ]
+  end
+end
