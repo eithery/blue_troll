57afd6aeac488c0b1bcde7864b75bcbc35f6dc0d
Michael Protsenko <michael.protsenko@gmail.com>
2014-04-25 19:14:25 -0400

    Created some spec for registering the new participant use case.

--- spec/features/user_registers_new_participant_spec.rb
+++ spec/features/user_registers_new_participant_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 
 describe "User creates new participant" do
+  let(:user) { FactoryGirl.create(:active_user) }
+
+  subject { page }
+  before do
+    sign_in user
+    click_link 'Register new participant'
+  end
+
+  user { should_be_signed_in }
 end
