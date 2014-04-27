1a4e3c760ba75a06e066505e6fb81da192693d30
Michael Protsenko <michael.protsenko@gmail.com>
2014-04-27 19:05:29 -0400

    Designed email for crew lead sent by register the new participant.

--- app/mailers/participants_mailer.rb
+++ app/mailers/participants_mailer.rb
@@ -1,5 +1,6 @@
 class ParticipantsMailer < ActionMailer::Base
   def created(participant)
+    @participant = participant
     mail to: participant.crew.emails, from: "Blue_Trolley <#{club_email}>",
       subject: "#{sender}: #{participant_created_subject}"
   end
