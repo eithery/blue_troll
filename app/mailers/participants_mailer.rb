0002bbe3d66281a8cd7108d26932da846a3d6a58
Michael Protsenko <michael.protsenko@gmail.com>
2014-04-27 21:31:26 -0400

    Made mailers specs and mailer classes more DRY.

--- app/mailers/participants_mailer.rb
+++ app/mailers/participants_mailer.rb
@@ -1,28 +1,32 @@
 class ParticipantsMailer < ActionMailer::Base
   def created(participant, crew)
-    @participant = participant
-    @crew = crew
-    target = [participant.user_account.email]
-    target << participant.email unless participant.email == participant.user_account.email
+    set_members participant, crew
     mail to: target, from: "Blue_Trolley <#{club_email}>",
       subject: "#{sender}: #{participant_created_subject}"
   end
 
 
   def approval_requested(participant, crew)
-    @participant = participant
-    @crew = crew
+    set_members participant, crew
     mail to: participant.crew.emails, from: "Blue_Trolley <#{club_email}>",
       subject: "#{sender}: #{approval_request_subject}"
   end
 
 
   def approved(participant, crew)
-    @participant = participant
-    @crew = crew
-    target = [participant.user_account.email]
-    target << participant.email unless participant.email == participant.user_account.email
+    set_members participant, crew
     mail to: target, from: "Blue_Trolley <#{club_email}>",
       subject: "#{sender}: #{participant_approved_subject}"
+  end
+
+private
+  def set_members(participant, crew)
+    @participant = participant
+    @crew = crew
+  end
+
+  def target
+    target = [@participant.user_account.email]
+    target << @participant.email unless @participant.email == @participant.user_account.email
   end
 end
--- spec/mailers/user_accounts_mailer_spec.rb
+++ spec/mailers/user_accounts_mailer_spec.rb
@@ -2,43 +2,39 @@ require "spec_helper"
 include MailersHelper
 
 shared_examples_for "user accounts mailer" do
-  it "sent to user email address" do
-    mail.to.should include(user.email)
-  end
-
-  it "sent from Blue Trolley club email address" do
-    mail.from.should include(club_email)
-  end
+  it { should be_sent_to user.email }
+  it { should be_sent_from club_email }
 end
 
 
 describe UserAccountsMailer do
   let(:user) { mock_user_account email: 'gwen@gmail1.com' }
+  subject { mail }
 
   describe "#registered" do
     let(:mail) { UserAccountsMailer.registered(user) }
-    specify { mail.subject.should == "#{sender}: #{registered_subject}" }
+    it { should have_subject "#{sender}: #{registered_subject}" }
     it_behaves_like "user accounts mailer"
   end
 
 
   describe "#activated" do
     let(:mail) { UserAccountsMailer.activated(user) }
-    specify { mail.subject.should == "#{sender}: #{activated_subject}" }
+    it { should have_subject "#{sender}: #{activated_subject}" }
     it_behaves_like "user accounts mailer"
   end
 
 
   describe "#password_reset" do
     let(:mail) { UserAccountsMailer.password_reset(user) }
-    specify { mail.subject.should == "#{sender}: #{password_reset_subject}" }
+    it { should have_subject "#{sender}: #{password_reset_subject}" }
     it_behaves_like "user accounts mailer"
   end
 
 
   describe "#password_changed" do
     let(:mail) { UserAccountsMailer.password_changed(user) }
-    specify { mail.subject.should == "#{sender}: #{password_changed_subject}" }
+    it { should have_subject "#{sender}: #{password_changed_subject}" }
     it_behaves_like "user accounts mailer"
   end
 end
