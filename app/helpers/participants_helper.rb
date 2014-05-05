8bdb09f185307e47818d5a17da89afd4ec6637ef
Michael Protsenko <michael.protsenko@gmail.com>
2014-05-05 19:11:24 -0400

    Extracted view functionality to helpers.

--- app/helpers/participants_helper.rb
+++ app/helpers/participants_helper.rb
@@ -7,4 +7,15 @@ module ParticipantsHelper
   def form_header
     "#{@participant.new_record? ? "New" : "Edit"} Participant"
   end
+
+
+  def approve_tag(participant)
+    link_to 'Accept', '#', class: "btn btn-success btn-xs" if can_approve? participant
+  end
+
+
+private
+  def can_approve?(participant)
+    current_user.crew_lead? && !participant.approved?
+  end
 end
