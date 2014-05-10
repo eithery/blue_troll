ce8feb3309b0e4238d841c7c6c2ca4853ee3e03d
Michael Protsenko <michael.protsenko@gmail.com>
2014-05-10 13:31:33 -0400

    Protected crew show page from other users.

--- app/controllers/crews_controller.rb
+++ app/controllers/crews_controller.rb
@@ -1,12 +1,19 @@
 class CrewsController < ApplicationController
   before_filter :signed_in_user
-  
+  before_filter :correct_user, only: [:show]
+
   def index
   	@crews = Crew.order(:name)
   end
 
 
   def show
+  end
+
+
+private
+  def correct_user
     @crew = Crew.find(params[:id])
+    redirect_to root_path unless current_user.admin? || @crew.lead?(current_user)
   end
 end
