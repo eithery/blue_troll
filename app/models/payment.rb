f0a78bfc3e50a79fddfd11da62c5cf7789000d64
Michael Protsenko <michael.protsenko@gmail.com>
2014-05-13 17:44:23 -0400

    Created email template for payment sent event.

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,5 +1,5 @@
 class Payment
-  attr_reader :payment_type, :amount, :notes
+  attr_reader :payer, :payment_type, :amount, :notes
 
   def initialize(payer, payment_options)
     @payer = payer
@@ -14,5 +14,13 @@ class Payment
     payees = @payer.participants.to_a.select { |p| p.unpaid? }
     payees.select! { |p| p.id == @payee_id } unless @payee_id.blank?
     payees
+  end
+
+
+  def payment_type_string
+    "Pay Pal" if payment_type.to_i == 0
+    "Check" if payment_type.to_i == 1
+    "Cash" if payment_type.to_i == 2
+    "Other"
   end
 end
