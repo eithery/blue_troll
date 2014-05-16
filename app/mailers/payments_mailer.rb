fc0b72c610684b305736ced47528673a8e61ac31
Michael Protsenko <michael.protsenko@gmail.com>
2014-05-16 18:45:31 -0400

    Implemented all paiment mailer actions.

--- app/mailers/payments_mailer.rb
+++ app/mailers/payments_mailer.rb
@@ -11,9 +11,27 @@ class PaymentsMailer < ActionMailer::Base
 
 
   def payment_received(participant)
+    target = [participant.user_account.email, participant.email].uniq
+    @participant = participant
+    mail to: target, from: "Blue_Trolley <#{club_email}>",
+      subject: "#{sender}: #{payment_received_subject}"
+  end
+
+
+  def payment_confirmation_requested(crew_lead, participant)
+    target = UserAccount.financier_emails
+    @crew_lead = crew_lead
+    @participant = participant
+    mail to: target.uniq, from: "Blue_Trolley <#{club_email}>",
+      subject: "#{sender}: #{payment_received_subject}"
   end
 
 
   def payment_confirmed(participant)
+    target = [participant.user_account.email, participant.email]
+    target += user.crew.emails
+    @participant = participant
+    mail to: target.uniq, from: "Blue_Trolley <#{club_email}>",
+      subject: "#{sender}: #{payment_confirmed_subject}"
   end
 end
