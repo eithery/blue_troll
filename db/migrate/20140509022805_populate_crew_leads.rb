bde081824c6cc8a810fd8daaabfd47f0b0f47f1f
Michael Protsenko <michael.protsenko@gmail.com>
2014-05-18 23:51:50 -0400

    Marked inactive crews in crew leads migration file.

--- db/migrate/20140509022805_populate_crew_leads.rb
+++ db/migrate/20140509022805_populate_crew_leads.rb
@@ -2,31 +2,31 @@ class PopulateCrewLeads < ActiveRecord::Migration
   def up
     @crews = [
       { name: 'Bells', native_name: 'Звон Вечерний' },
-      { name: 'Carolinian Robbins', native_name: 'Каролинские красношейки' },
+      { name: 'Carolinian Robbins', native_name: 'Каролинские красношейки', active: false },
       { name: 'Drummers', native_name: 'Барабанщики' },
       { name: 'Dubki', native_name: 'Дубки' },
       { name: 'Elki-Palki', native_name: 'Ёлки-Палки' },
       { name: 'El-Tor', native_name: 'Эль-Тор' },
       { name: 'Green Carriage', native_name: 'Зеленая Карета' },
-      { name: 'GTO', native_name: 'ГТО' },
+      { name: 'GTO', native_name: 'ГТО', active: false },
       { name: 'Guests', native_name: 'Гости Слета' },
       { name: 'Happy Crew', native_name: 'Веселые Ребята' },
       { name: 'Homeless', native_name: 'Беспризорники' },
       { name: 'Inner Voice', native_name: 'Внутренний Голос' },
       { name: 'Kazaki-Razboiniki', native_name: 'Казаки-Разбойники' },
       { name: 'Left Coast', native_name: 'Левый Берег' },
-      { name: 'Mandamus', native_name: 'Мандамус' },
+      { name: 'Mandamus', native_name: 'Мандамус', active: false },
       { name: 'My People', native_name: 'Свои Люди' },
       { name: 'North People', native_name: 'Люди Севера', active: false },
-      { name: 'Okhotnyj Ryad', native_name: 'Охотный ряд' },
+      { name: 'Okhotnyj Ryad', native_name: 'Охотный ряд', active: false },
       { name: 'Palata #6', native_name: 'Палата №6' },
       { name: 'Pegassus', native_name: 'Пегас' },
-      { name: 'Pshik & Co', native_name: 'Пшик и компания' },
-      { name: 'Sentyabryata', native_name: 'Сентябрята' },
+      { name: 'Pshik & Co', native_name: 'Пшик и компания', active: false },
+      { name: 'Sentyabryata', native_name: 'Сентябрята', active: false },
       { name: 'Station Razliv', native_name: 'Станция Разлив' },
       { name: 'Stranniki', native_name: 'Странники' },
       { name: 'Tom Sawyer', native_name: 'Том Сойер' },
-      { name: 'Trjam', native_name: 'ТRЯМ' },
+      { name: 'Trjam', native_name: 'ТРЯМ' },
       { name: 'Trojan Donkeys', native_name: 'Троянские Ослики' },
       { name: 'White Crow', native_name: 'Белая Ворона' },
       { name: "Wolf's Tail", native_name: 'Шайка-Лейка Волчий Хвост' },
@@ -121,9 +121,14 @@ class PopulateCrewLeads < ActiveRecord::Migration
 
     crew_leads.each_with_index do |lead, index|
       lead[:user_account] = UserAccount.find_by_login @user_accounts[index][:login]
-      lead[:approved_at] = lead[:payment_confirmed_at] = Time.now
+      lead[:approved_at] = Time.now
       lead[:approved_by] = 'admin'
-      lead[:payment_confirmed_by] = 'ryeliseyev'
+
+      if lead[:user_account].crew.active?
+        lead[:payment_confirmed_at] = Time.now
+        lead[:payment_confirmed_by] = 'ryeliseyev'
+      end
+
       lead[:created_by] = 'admin'
       lead[:updated_by] = 'admin'
     end
