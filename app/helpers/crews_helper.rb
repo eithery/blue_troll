d0e3eb1058c60c009f5d33e75c16c667065aaea3
Michael Protsenko <michael.protsenko@gmail.com>
2014-05-19 21:49:42 -0400

    Reused participant related partials. Removed code duplication.

--- app/helpers/crews_helper.rb
+++ app/helpers/crews_helper.rb
@@ -3,7 +3,31 @@ module CrewsHelper
     crew.leads.map { |lead| lead.login }.join(', ')
   end
 
+
   def crew_emails(crew)
     crew.emails.join(', ')
+  end
+
+
+  def show_participants_tag(crew)
+    link_to image_tag('participants.png', alt: 'Participants', class: "bt-img bt-img-reduced"),
+      crew_path(crew), title: 'Click here to see all participants in the crew.', rel: 'tooltip'
+  end
+
+
+  def add_participant_tag(crew)
+    link_to image_tag('new_participant.png', alt: 'Add Participant', class: "bt-img bt-img-reduced"),
+      new_participant_path(crew_id: crew.id), title: 'Click here to add a new participant.', rel: 'tooltip'
+  end
+
+
+  def download_tickets_tag(crew)
+    link_to image_tag('ticket.png', alt: 'Generate ticket', class: "bt-img bt-img-reduced"),
+      crew_tickets_path(crew_id: crew), title: 'Click here to download tickets for the crew.', rel: 'tooltip'
+  end
+
+
+  def totals(crew)
+    "#{crew.total_participants} (#{crew.total_adults}/#{crew.total_children}/#{crew.total_babies})"
   end
 end
