e92d7b8188eee52c293b1dbf263f02c95735421e
Michael Protsenko <michael.protsenko@gmail.com>
2014-05-19 22:20:08 -0400

    Added special images for crew leads and babies.

--- app/helpers/participants_helper.rb
+++ app/helpers/participants_helper.rb
@@ -50,11 +50,35 @@ module ParticipantsHelper
 
 
   def delete_tag(participant)
-    unless participant.payment_confirmed?
+    if !participant.payment_confirmed? || current_user.admin?
       link_to image_tag('delete.png', alt: 'Delete participant', class: "bt-img"),
         participant_path(participant), method: :delete, rel: 'tooltip',
         title: "Click here to delete #{participant.display_name} from Blue Trolley participants list",
         data: { confirm: "Are you sure to delete #{participant.display_name} from the participants list?" }
     end
+  end
+
+
+  def crew_lead_image(participant)
+    if participant.user_account.crew_lead? && participant.primary?
+      image_tag 'crew_lead.png', class: "bt-img-reduced", style: "margin-left: 0.25em; margin-bottom: 0.2em;"
+    end
+  end
+
+
+  def baby_image(participant)
+    if participant.age_category == AgeCategory::BABY
+      image_tag 'baby.png', class: "bt-img-reduced", style: "margin-left: 0.25em;"
+    end
+  end
+
+
+  def age_category(participant)
+    age = participant.age || 0
+    age = participant.age_category == AgeCategory::BABY ? '?' : '' if age == 0
+
+    return "B#{age}" if participant.age_category == AgeCategory::BABY
+    return "C#{age}" if participant.age_category == AgeCategory::CHILD
+    return "A#{age}"
   end
 end
