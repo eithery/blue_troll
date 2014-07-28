c4bbd036fdfbab6aa1939b4a967eeec70979aa2c
Michael Protsenko <michael.protsenko@gmail.com>
2014-07-28 00:32:24 -0400

    Implemented the action event_announcement in static pages controller downloading and displaying event announcement document.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,4 +5,10 @@ class StaticPagesController < ApplicationController
 
   def statistics
   end
+
+
+  def event_announcement
+    send_file Rails.root.join('app', 'assets', 'docs', 'event_announcement.pdf'), file_name: 'event_announcement.pdf',
+      type: 'application/pdf', disposition: 'inline'
+  end
 end
