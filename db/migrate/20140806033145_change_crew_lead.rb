25cc14a3a0027de4b4911d06adf1e34c38471eae
Michael Protsenko <michael.protsenko@gmail.com>
2014-09-15 23:26:52 -0400

    Temporary commented migration making Vbelyaev as crew lead.

--- db/migrate/20140806033145_change_crew_lead.rb
+++ db/migrate/20140806033145_change_crew_lead.rb
@@ -1,18 +1,18 @@
 class ChangeCrewLead < ActiveRecord::Migration
   def up
-    prev_lead = UserAccount.find_by_login('abelyaev')
-    prev_lead.crew_lead = false
-    prev_lead.save(validate: false)
+#    prev_lead = UserAccount.find_by_login('abelyaev')
+#    prev_lead.crew_lead = false
+#    prev_lead.save(validate: false)
 
-    email = 'vadim@belyayev.com'
-    password = SecureRandom.urlsafe_base64
-    new_lead = UserAccount.create!(login: 'vbelyaev', email: email, email_confirmation: email, password: password,
-      password_confirmation: password, crew: prev_lead.crew, crew_lead: true, active: true, activated_at: Time.now)
+#    email = 'vadim@belyayev.com'
+#    password = SecureRandom.urlsafe_base64
+#    new_lead = UserAccount.create!(login: 'vbelyaev', email: email, email_confirmation: email, password: password,
+#      password_confirmation: password, crew: prev_lead.crew, crew_lead: true, active: true, activated_at: Time.now)
 
-    p = Participant.where(last_name: 'Belyaev', first_name: 'Vadim').to_a.first
-    p.primary = true
-    p.user_account = new_lead
-    p.save
+#    p = Participant.where(last_name: 'Belyaev', first_name: 'Vadim').to_a.first
+#    p.primary = true
+#    p.user_account = new_lead
+#    p.save
   end
 
 
