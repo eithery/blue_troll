09e6dff66a2fc63c4a4acfb17c14e1627cc6d2eb
Michael Protsenko <michael.protsenko@gmail.com>
2015-04-12 21:43:27 -0400

    Configured 'should' RSpec mode. Required RSpec collection matchers and its gem.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,9 @@ require 'spork'
 Spork.prefork do
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/collection_matchers'
+  require 'rspec/its'
   require 'rspec/rails'
-  require 'rspec/autorun'
 
   Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
@@ -15,7 +16,15 @@ Spork.prefork do
     config.fixture_path = "#{::Rails.root}/spec/fixtures"
     config.use_transactional_fixtures = true
     config.infer_base_class_for_anonymous_controllers = false
+    config.infer_spec_type_from_file_location!
     config.order = "random"
+
+    config.expect_with :rspec do |c|
+      c.syntax = [:should, :expect]
+    end
+    config.mock_with :rspec do |mocks|
+      mocks.syntax = [:should, :expect]
+    end
 
     # Includes Capybara domain specific language.
     config.include Capybara::DSL
