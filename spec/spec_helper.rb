46c12c6c11c83fa68ffd81821d6c30093cda88ff
Michael Protsenko <michael.protsenko@gmail.com>
2015-04-19 16:09:39 -0400

    Removed Spork initialization from spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,48 +1,41 @@
 require 'rubygems'
-require 'spork'
 
-Spork.prefork do
-  ENV["RAILS_ENV"] ||= 'test'
-  require File.expand_path("../../config/environment", __FILE__)
-  require 'rspec/collection_matchers'
-  require 'rspec/its'
-  require 'rspec/rails'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/collection_matchers'
+require 'rspec/its'
+require 'rspec/rails'
 
-  Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
-  ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
-  RSpec.configure do |config|
-    config.fixture_path = "#{::Rails.root}/spec/fixtures"
-    config.use_transactional_fixtures = true
-    config.infer_base_class_for_anonymous_controllers = false
-    config.infer_spec_type_from_file_location!
-    config.order = "random"
-    config.render_views
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.infer_base_class_for_anonymous_controllers = false
+  config.infer_spec_type_from_file_location!
+  config.order = "random"
+  config.render_views
 
-    config.expect_with :rspec do |c|
-      c.syntax = [:should, :expect]
-    end
-    config.mock_with :rspec do |mocks|
-      mocks.syntax = [:should, :expect]
-    end
-
-    # Includes Capybara domain specific language.
-    config.include Capybara::DSL
-
-    config.include SessionsHelper, type: :view
-
-    config.include DomainModelMocks
-    config.include DomainModelMatchers
-    config.include FlashMatchers
-    config.include MailerMatchers
-    config.include PageMatchers
-    config.include ViewMatchers
-
-    config.extend UserAccountMacros
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
   end
-end
+  config.mock_with :rspec do |mocks|
+    mocks.syntax = [:should, :expect]
+  end
 
+  # Includes Capybara domain specific language.
+  config.include Capybara::DSL
 
-Spork.each_run do
+  config.include SessionsHelper, type: :view
+
+  config.include DomainModelMocks
+  config.include DomainModelMatchers
+  config.include FlashMatchers
+  config.include MailerMatchers
+  config.include PageMatchers
+  config.include ViewMatchers
+
+  config.extend UserAccountMacros
 end
