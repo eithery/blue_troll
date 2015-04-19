60bd5baaa6bf567288d72b8e3046c685b6b1218b
Michael Protsenko <michael.protsenko@gmail.com>
2015-04-19 18:05:35 -0400

    Removed automatic view rendering from RSpec config. Raise errors for deprecations.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,8 +15,8 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
   config.infer_spec_type_from_file_location!
+  config.raise_errors_for_deprecations!
   config.order = "random"
-  config.render_views
 
   config.expect_with :rspec do |c|
     c.syntax = [:should, :expect]
