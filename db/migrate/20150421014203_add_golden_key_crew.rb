26f9287f2117f77577b97c70c6690d78c52f35cd
Michael Protsenko <michael.protsenko@gmail.com>
2015-04-20 23:11:20 -0400

    Modified spelling of the new crew lead.

--- db/migrate/20150421014203_add_golden_key_crew.rb
+++ db/migrate/20150421014203_add_golden_key_crew.rb
@@ -4,13 +4,13 @@ class AddGoldenKeyCrew < ActiveRecord::Migration
 
     email = 'yellowyuli@gmail.com'
     password = SecureRandom.urlsafe_base64
-    user = UserAccount.create!(login: 'jshpilman', email: email, email_confirmation: email, password: password,
+    user = UserAccount.create!(login: 'yshpilman', email: email, email_confirmation: email, password: password,
       password_confirmation: password, crew: crew, crew_lead: true, active: true, activated_at: Time.now)
   end
 
 
   def down
-    user = UserAccount.find_by_login('jshpilman')
+    user = UserAccount.find_by_login('yshpilman')
     crew = user.crew
     user.destroy
     crew.destroy
