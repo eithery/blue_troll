0ea013e11d28f992a52a05bf54c96f2dd1a8010e
Michael Protsenko <michael.protsenko@gmail.com>
2016-01-31 23:36:47 -0500

    Updated Rails project files to Rails 5.

--- config/application.rb
+++ config/application.rb
@@ -1,19 +1,15 @@
 require File.expand_path('../boot', __FILE__)
 
-# Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+Bundler.require(*Rails.groups)
 
 module BlueTroll
   class Application < Rails::Application
-    config.i18n.enforce_available_locales = true
-    config.autoload_paths << "#{Rails.root}/app/enumerations"
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
   end
 end
