039a7b458dfc6abc032a81c1a1f4339dc19a7aaf
Michael Protsenko <michael.protsenko@gmail.com>
2016-02-15 01:22:29 -0500

    Fixed Participant age validation.

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -5,7 +5,7 @@ class Participant < ApplicationRecord
   validates :last_name, :first_name, presence: true
   validates :ticket_code, uniqueness: true, length: { minimum: 10 }
   validates :user_account, presence: true
-  validates :age, presence: true, unless: Proc.new { |p| p.age_category == AgeCategory::ADULT }
+  validates :age, presence: true, unless: Proc.new { |p| p.adult? }
   validates :age, numericality: true, allow_blank: true
   validates :email, format: { with: UserAccount::VALID_EMAIL_REGEX }, allow_blank: true
 
