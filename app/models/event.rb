4ab6e113a550e8a177f834ee3e5a86d2a29e166e
Michael Protsenko <michael.protsenko@gmail.com>
2016-02-18 15:18:38 -0500

    Implemented Event model. Created the set of specs for Event model.

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,14 +6,25 @@ class Event < ApplicationRecord
   include NameHolder, Trackable
 
   belongs_to :event_type
-  has_many :crews, class_name: EventCrew
-  has_many :participants, class_name: EventParticipant
+  has_many :crews, class_name: EventCrew, dependent: :restrict_with_exception
+  has_many :participants, through: :crews, class_name: EventParticipant
 
-  validates :event_type, :started_on, :finished_on, presence: true
+  validates :started_on, :finished_on, presence: true
   validates :address, presence: true, length: { maximum: 255 }
+
+  validate :dates_mismatch
 
 
   def participant_by_ticket(ticket_code)
-#    Participant.all.each { |p| return p if p.ticket_code.to_i(16).to_s == ticket_code.strip }
+    participants.find { |p| p.ticket_code.to_i(16).to_s == ticket_code.strip }
+  end
+
+
+private
+
+  def dates_mismatch
+    if !started_on.nil? && !finished_on.nil? && started_on > finished_on
+      errors[:base] << "Event start date can't be later than end date"
+    end
   end
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -4,7 +4,8 @@
 require 'rails_helper'
 
 describe Event do
-  subject { FactoryGirl.build :event }
+  subject(:event) { FactoryGirl.build :event }
+  let(:event_with_crews) { FactoryGirl.create :event, :with_crews }
 
   it_behaves_like 'a valid domain model'
   it_behaves_like 'it has a required unique name'
@@ -14,15 +15,63 @@ describe Event do
   it { should respond_to :address }
   it { should respond_to :notes }
   it { should respond_to :event_type }
-  it { should respond_to :crews }
+  it { should respond_to :crews, :participants }
+  it { should respond_to :participant_by_ticket }
 
   it { should validate_presence_of :started_on }
   it { should validate_presence_of :finished_on }
   it { should validate_presence_of :address }
-  it { should validate_presence_of :event_type }
   it { should validate_length_of(:address).is_at_most 255 }
 
   it { should belong_to :event_type }
   it { should have_db_index :event_type_id }
-  it { should have_many(:crews).class_name EventCrew }
+  it { should have_many(:crews).class_name(EventCrew).dependent :restrict_with_exception }
+  it { should have_many(:participants).through(:crews).class_name EventParticipant }
+
+
+  describe 'validation' do
+    context 'when the event end date less than a start date' do
+      before do
+        event.started_on = Date.new(2016, 03, 03)
+        event.finished_on = Date.new(2016, 03, 01)
+      end
+
+      it { is_expected.to_not be_valid }
+      it { is_expected.to have(1).error_on :base }
+    end
+
+    context 'when the event start date same as end date but later by time' do
+      before do
+        event.started_on = DateTime.now
+        event.finished_on = event.started_on.to_date
+      end
+
+      it { is_expected.to be_valid }
+    end
+
+    context 'when event type is not defined' do
+      before { event.event_type = nil }
+
+      it { is_expected.to_not be_valid }
+      it { is_expected.to have(1).error_on :event_type }
+    end
+  end
+
+
+  describe '#crews' do
+    it { expect(event_with_crews).to have(2).crews }
+    it { expect(Event.new).to have(:no).crews }
+  end
+
+
+  describe '#participants' do
+    include_context 'prepared event'
+
+    it { expect(event).to have(5).participants }
+    it { expect(Event.new).to have(:no).participants }
+  end
+
+
+  describe '#participant_by_ticket' do
+  end
 end
