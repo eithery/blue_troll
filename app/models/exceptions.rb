18786f2844f7e16ed4aa23a7dfbd44fd51808cdf
Michael Protsenko <michael.protsenko@gmail.com>
2016-02-21 21:16:40 -0500

    Created a module containing custom app exceptions.

---
+++ app/models/exceptions.rb
@@ -0,0 +1,10 @@
+# Eithery Lab, 2016.
+# Custom Blue Troll exceptions.
+
+module Exceptions
+  class InvalidCrewLeadError < StandardError
+  end
+
+  class InvalidFinancierError < StandardError
+  end
+end
