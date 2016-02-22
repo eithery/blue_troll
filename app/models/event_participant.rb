67c3bd4128b0ac1457aa4fa2f81229d23d88aaf7
Michael Protsenko <michael.protsenko@gmail.com>
2016-02-21 21:21:58 -0500

    Completely implemented and covered by spec EventParticipant model.

--- app/models/event_participant.rb
+++ app/models/event_participant.rb
@@ -8,10 +8,24 @@ class EventParticipant < ApplicationRecord
   belongs_to :crew, class_name: EventCrew, foreign_key: :event_crew_id, inverse_of: :participants
   belongs_to :person, class_name: Participant, foreign_key: :participant_id
 
-  validates :ticket_code, uniqueness: true, length: { is: 10 }, if: ->(p){ p.approved? }
+  validates :ticket_code, uniqueness: { case_sensitive: false }, length: { is: 10 }, if: ->(p){ p.approved? }
+
+  AWAITING_APPROVAL = 'Await crew lead approval'
+  ACCEPTED = 'Accepted'
+  PAYMENT_SENT = 'Payment sent'
+  PAYMENT_RECEIVED = 'Payment received'
+  PAYMENT_CONFIRMED = 'Paid'
+
+  enum payment_type: [:paypal, :check, :cash, :other]
 
 
   def event
+    crew.event
+  end
+
+
+  def name
+    person.display_name
   end
 
 
@@ -26,7 +40,7 @@ class EventParticipant < ApplicationRecord
 
 
   def unpaid?
-    !payment_sent? && !payment_received? && !payment_confirmed?
+    !paid?
   end
 
 
@@ -50,32 +64,36 @@ class EventParticipant < ApplicationRecord
   end
 
 
-  def approve(crew_lead)
-#    update_attributes(approved_at: Time.now, approved_by: crew_lead.login)
+  def approve(approver)
+    validate_crew_lead approver, 'Approver'
+    generate_ticket_code
+    update_attributes(ticket_code: ticket_code, approved_at: Time.now, approved_by: approver.name)
   end
 
 
   def send_payment(payment)
-#    update_attributes(payment_sent_at: Time.now, payment_sent_by: self.user_account.login,
-#      payment_type: payment.payment_type, payment_notes: payment.notes)
+    update_attributes(payment_sent_at: Time.now, payment_sent_by: payment.payer.name,
+      payment_type: payment.payment_type, payment_notes: payment.notes)
   end
 
 
-  def receive_payment(amount, crew_lead)
-#    update_attributes(payment_received_at: Time.now, payment_received_by: crew_lead.login)
+  def receive_payment(receiver)
+    validate_crew_lead receiver, 'Payment receiver'
+    update_attributes(payment_received_at: Time.now, payment_received_by: receiver.name)
   end
 
 
-  def confirm_payment(amount, financier)
-#    update_attributes(payment_confirmed_at: Time.now, payment_confirmed_by: financier.login)
+  def confirm_payment(confirmer)
+    validate_financier confirmer
+    update_attributes(payment_confirmed_at: Time.now, payment_confirmed_by: confirmer.name)
   end
 
 
   def status
-    return 'Paid' if payment_confirmed?
-    return 'Payment received' if payment_received?
-    return 'Payment sent' if payment_sent?
-    return approved? ? 'Accepted' : 'Await crew lead approval'
+    return PAYMENT_CONFIRMED if payment_confirmed?
+    return PAYMENT_RECEIVED if payment_received?
+    return PAYMENT_SENT if payment_sent?
+    return approved? ? ACCEPTED : AWAITING_APPROVAL
   end
 
 
@@ -83,5 +101,18 @@ private
 
   def generate_ticket_code
       self.ticket_code = SecureRandom.hex[0, 10] if ticket_code.blank?
+  end
+
+
+  def validate_crew_lead(crew_lead, title)
+    raise Exceptions::InvalidCrewLeadError, 'Crew lead is not specified' if crew_lead.blank?
+    raise Exceptions::InvalidCrewLeadError, "#{title} is not a crew lead" unless crew_lead.crew_lead?
+    raise Exceptions::InvalidCrewLeadError, 'Crew lead belongs to another crew' if crew_lead.crew != crew
+  end
+
+
+  def validate_financier(financier)
+    raise Exceptions::InvalidFinancierError, 'Financier is not specified' if financier.blank?
+    raise Exceptions::InvalidFinancierError, 'Payment confirmer is not a financier' unless financier.financier?
   end
 end
--- spec/models/event_participant_spec.rb
+++ spec/models/event_participant_spec.rb
@@ -9,7 +9,7 @@ describe EventParticipant do
   it_behaves_like 'a valid domain model'
   it_behaves_like 'it has timestamps'
 
-  it { should respond_to :event, :crew, :person }
+  it { should respond_to :event, :crew, :person, :name }
   it { should respond_to :ticket_code }
   it { should respond_to :crew_lead?, :financier?, :gatekeeper? }
   it { should respond_to :flagged?, :notes }
@@ -30,30 +30,58 @@ describe EventParticipant do
 
 
   describe 'validation' do
+    context 'when a participant crew is not specified' do
+      before { participant.crew = nil }
+
+      it { is_expected.to_not be_valid }
+      it { is_expected.to have(1).error_on :crew }
+    end
+
+    context 'when a person is not specified' do
+      before { participant.person = nil }
+
+      it { is_expected.to_not be_valid }
+      it { is_expected.to have(1).error_on :person }
+    end
   end
 
 
   describe '#event' do
+    it 'is retrieved from a crew' do
+      expect(participant.event).to eq participant.crew.event
+    end
+  end
+
+
+  describe '#name' do
+    it { expect(participant.name).to eq participant.person.display_name }
   end
 
 
   describe '#ticket_code' do
-    context 'when a just created participant' do
+    context 'for a just created participant' do
+      it { expect(participant.ticket_code).to be nil }
     end
 
     context 'when a participant is approved' do
-#      it { should validate_length_of(:ticket_code).is_exactly(10).symbols }
+      include_context 'upcoming event'
+      before { participant.approve crew_lead }
+
+      it { expect(participant.ticket_code).to_not be nil }
+      it { expect(participant.ticket_code).to have(10).characters }
+      it { should validate_length_of(:ticket_code).is_equal_to 10 }
+      it { should validate_uniqueness_of(:ticket_code).case_insensitive }
     end
   end
 
 
   describe '#approved?' do
-    context "when NOT approved by a crew lead" do
+    context 'when NOT approved by a crew lead' do
       before { participant.approved_at = nil }
       it { expect(participant.approved?).to be false }
     end
 
-    context "when approved by a crew lead" do
+    context 'when approved by a crew lead' do
       before { participant.approved_at = Time.now }
       it { expect(participant.approved?).to be true }
     end
@@ -61,14 +89,51 @@ describe EventParticipant do
 
 
   describe '#checked_in?' do
+    context 'when NOT checked in the gate' do
+      before { participant.checked_in_at = nil }
+      it { expect(participant.checked_in?).to be false }
+    end
+
+    context 'when checked in the gate' do
+      before { participant.checked_in_at = Time.now }
+      it { expect(participant.checked_in?).to be true }
+    end
   end
 
 
-  describe '#unpaid?' do
-  end
+  describe '#paid?, #unpaid?' do
+    context 'for a new participant' do
+      it { expect(EventParticipant.new.paid?).to be false }
+      it { expect(EventParticipant.new.unpaid?).to be true }
+    end
 
+    context 'when a payment was sent' do
+      before { participant.payment_sent_at = Time.now }
 
-  describe '#paid?' do
+      it { expect(participant.paid?).to be false }
+      it { expect(participant.unpaid?).to be true }
+    end
+
+    context 'when a payment was sent and received by crew lead' do
+      before do
+        participant.payment_sent_at = Time.now
+        participant.payment_received_at = Time.now
+      end
+
+      it { expect(participant.paid?).to be false }
+      it { expect(participant.unpaid?).to be true }
+    end
+
+    context 'when a payment was sent, received, and confirmed by a crew lead' do
+      before do
+        participant.payment_sent_at = Time.now
+        participant.payment_received_at = Time.now
+        participant.payment_confirmed_at = Time.now
+      end
+
+      it { expect(participant.paid?).to be true }
+      it { expect(participant.unpaid?).to be false }
+    end
   end
 
 
@@ -112,54 +177,175 @@ describe EventParticipant do
 
 
   describe '#approve' do
-#    before do
-#      gwen.approve crew_lead
-#      gwen.reload
-#    end
+    include_context 'upcoming event'
 
-#    its(:approved?) { should be true }
-#    its(:approved_at) { should_not be_blank }
-#    its(:approved_by) { should == crew_lead.login }
+    context 'for newly created participant' do
+      it { expect(participant).to_not be_approved }
+      it { expect(participant.approved_at).to be nil }
+      it { expect(participant.approved_by).to be nil }
+    end
+
+    context 'when approver is a valid crew lead' do
+      before do
+        participant.approve crew_lead
+        participant.reload
+      end
+
+      it { expect(participant).to be_approved }
+      it { expect(participant.approved_at).to_not be_blank }
+      it { expect(participant.approved_by).to eq crew_lead.name }
+    end
+
+    context 'when a crew lead is NOT specified' do
+      it { expect { participant.approve nil }.to raise_error Exceptions::InvalidCrewLeadError,
+        'Crew lead is not specified' }
+    end
+
+    context 'when an approver belongs to another crew' do
+      it { expect { participant.approve other_crew_lead }.to raise_error Exceptions::InvalidCrewLeadError,
+        'Crew lead belongs to another crew' }
+    end
+
+    context 'when an approver is not a crew lead' do
+      it { expect { participant.approve participant }.to raise_error Exceptions::InvalidCrewLeadError,
+        'Approver is not a crew lead' }
+    end
   end
 
 
   describe '#send_payment' do
-#    before do
-#      payment = Payment.new(gwen.user_account, amount: 45.0, payment_type: PaymentType::CASH)
-#      gwen.send_payment payment
-#      gwen.reload
-#    end
+    include_context 'upcoming event'
 
-#    its(:payment_sent?) { should be true }
-#    its(:payment_sent_at) { should_not be_blank }
-#    its(:payment_sent_by) { should == gwen.user_account.login }
+    context 'for newly created participant' do
+      it { expect(participant).to_not be_payment_sent }
+      it { expect(participant.payment_sent_at).to be nil }
+      it { expect(participant.payment_sent_by).to be nil }
+    end
+
+    context 'when a payment is sent' do
+      let(:payer) { participant }
+
+      before do
+        payment = Payment.new payer, amount: 45.0, payment_type: Payment::CASH
+        participant.send_payment payment
+        participant.reload
+      end
+
+      it { expect(participant).to be_payment_sent }
+      it { expect(participant.payment_sent_at).to_not be_blank }
+      it { expect(participant.payment_sent_by).to eq payer.name }
+      it { expect(participant.cash?).to be true }
+    end
   end
 
 
   describe '#receive_payment' do
-#    before do
-#      gwen.receive_payment 45.00, crew_lead
-#      gwen.reload
-#    end
+    include_context 'upcoming event'
 
-#    its(:payment_received?) { should be true }
-#    its(:payment_received_at) { should_not be_blank }
-#    its(:payment_received_by) { should == crew_lead.login }
+    context 'for newly created participant' do
+      it { expect(participant).to_not be_payment_received }
+      it { expect(participant.payment_received_at).to be nil }
+      it { expect(participant.payment_received_by).to be nil }
+    end
+
+    context 'when a payment is received' do
+      before do
+        participant.receive_payment crew_lead
+        participant.reload
+      end
+
+      it { expect(participant).to be_payment_received }
+      it { expect(participant.payment_received_at).to_not be_blank }
+      it { expect(participant.payment_received_by).to eq crew_lead.name }
+    end
+
+    context 'when a crew lead is NOT specified' do
+      it { expect { participant.receive_payment nil }.to raise_error Exceptions::InvalidCrewLeadError,
+        'Crew lead is not specified' }
+    end
+
+    context 'when a payment receiver belongs to another crew' do
+      it { expect { participant.receive_payment other_crew_lead }.to raise_error Exceptions::InvalidCrewLeadError,
+        'Crew lead belongs to another crew' }
+    end
+
+    context 'when a payment receiver is not a crew lead' do
+      it { expect { participant.receive_payment participant }.to raise_error Exceptions::InvalidCrewLeadError,
+        'Payment receiver is not a crew lead' }
+    end
   end
 
 
   describe '#confirm_payment' do
-#    before do
-#      gwen.confirm_payment 45.00, financier
-#      gwen.reload
-#    end
+    include_context 'upcoming event'
 
-#    its(:payment_confirmed?) { should be true }
-#    its(:payment_confirmed_at) { should_not be_blank }
-#    its(:payment_confirmed_by) { should == financier.login }
+    context 'for newly created participant' do
+      it { expect(participant).to_not be_payment_confirmed }
+      it { expect(participant.payment_confirmed_at).to be nil }
+      it { expect(participant.payment_confirmed_by).to be nil }
+    end
+
+    context 'when a payment is confirmed' do
+      before do
+        participant.confirm_payment financier
+        participant.reload
+      end
+
+      it { expect(participant).to be_payment_confirmed }
+      it { expect(participant.payment_confirmed_at).to_not be_blank }
+      it { expect(participant.payment_confirmed_by).to eq financier.name }
+    end
+
+    context 'when a financier is NOT specified' do
+      it { expect { participant.confirm_payment nil }.to raise_error Exceptions::InvalidFinancierError,
+        'Financier is not specified' }
+    end
+
+    context 'when payment confirmer is not a financier' do
+      it { expect { participant.confirm_payment participant }.to raise_error Exceptions::InvalidFinancierError,
+        'Payment confirmer is not a financier' }
+    end
   end
 
 
   describe '#status' do
+    context 'for a new participant' do
+      it { expect(participant.status).to eq EventParticipant::AWAITING_APPROVAL }
+    end
+
+    context 'when a participant is approved' do
+      before { participant.approved_at = Time.now }
+      it { expect(participant.status).to eq EventParticipant::ACCEPTED }
+    end
+
+    context 'when a payment was sent' do
+      before do
+        participant.approved_at = Time.now
+        participant.payment_sent_at = Time.now
+      end
+
+      it { expect(participant.status).to eq EventParticipant::PAYMENT_SENT }
+    end
+
+    context 'when a payment was sent and received by crew lead' do
+      before do
+        participant.approved_at = Time.now
+        participant.payment_sent_at = Time.now
+        participant.payment_received_at = Time.now
+      end
+
+      it { expect(participant.status).to eq EventParticipant::PAYMENT_RECEIVED }
+    end
+
+    context 'when a payment was sent, received, and confirmed by a crew lead' do
+      before do
+        participant.approved_at = Time.now
+        participant.payment_sent_at = Time.now
+        participant.payment_received_at = Time.now
+        participant.payment_confirmed_at = Time.now
+      end
+
+      it { expect(participant.status).to eq EventParticipant::PAYMENT_CONFIRMED }
+    end
   end
 end
