16d9d1b73f31f361434c13422fb7ed0169f6379a
Michael Protsenko <michael.protsenko@gmail.com>
2016-02-27 00:04:46 -0500

    Increased specs performance using trancient model instances.

--- spec/support/shared_contexts.rb
+++ spec/support/shared_contexts.rb
@@ -6,41 +6,43 @@ require 'rails_helper'
 shared_context 'upcoming event' do
   let(:persons) do
     Array.new(7) do
-      user = FactoryGirl.create :user_account
-      FactoryGirl.create :participant, user_account: user
+      user = FactoryGirl.build :user_account
+      FactoryGirl.build :participant, user_account: user
     end
   end
 
   let(:crews) do
-    Array.new(2) { FactoryGirl.create :crew }
+    Array.new(2) { FactoryGirl.build :crew }
   end
 
   let(:event) do
-    event = FactoryGirl.create(:event)
-    crew = FactoryGirl.create(:event_crew, event: event, prototype: crews[0])
-    other_crew = FactoryGirl.create(:event_crew, event: event, prototype: crews[1])
+    event = FactoryGirl.build(:event)
+    crew = FactoryGirl.build(:event_crew, event: event, prototype: crews[0])
+    other_crew = FactoryGirl.build(:event_crew, event: event, prototype: crews[1])
 
-    participant = FactoryGirl.create(:event_participant, person: persons[0], crew: crew)
-    other_participant = FactoryGirl.create(:event_participant, person: persons[1], crew: crew)
-    crew_lead = FactoryGirl.create(:crew_lead, person: persons[2], crew: crew)
+    participant = FactoryGirl.build(:event_participant, person: persons[0], crew: crew)
+    other_participant = FactoryGirl.build(:event_participant, person: persons[1], crew: crew)
+    crew_lead = FactoryGirl.build(:crew_lead, person: persons[2], crew: crew)
     crew.participants << [participant, other_participant, crew_lead]
 
-    other_crew_lead = FactoryGirl.create(:crew_lead, person: persons[3], crew: other_crew)
-    financier = FactoryGirl.create(:financier, person: persons[4], crew: other_crew)
-    other_crew.participants << [other_crew_lead, financier]
+    other_crew_lead = FactoryGirl.build(:crew_lead, person: persons[3], crew: other_crew)
+    financier = FactoryGirl.build(:financier, person: persons[4], crew: other_crew)
 
+    other_crew.participants << [other_crew_lead, financier]
+    event.crews << [crew, other_crew]
     event.save!
     event
   end
 
   let(:other_event) do
-    event = FactoryGirl.create(:event)
-    crew = FactoryGirl.create(:event_crew, event: event, prototype: crews[0])
+    event = FactoryGirl.build(:event)
+    crew = FactoryGirl.build(:event_crew, event: event, prototype: crews[0])
 
-    other_event_crew_lead = FactoryGirl.create :crew_lead, person: persons[5], crew: crew
-    financier = FactoryGirl.create :financier, person: persons[6], crew: crew
+    other_event_crew_lead = FactoryGirl.build :crew_lead, person: persons[5], crew: crew
+    financier = FactoryGirl.build :financier, person: persons[6], crew: crew
+
     crew.participants << [other_event_crew_lead, financier]
-
+    event.crews << crew
     event.save!
     event
   end
