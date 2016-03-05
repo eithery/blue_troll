ba287202061c5f15e1fbdfcdbe7a8de2a0178b93
Michael Protsenko <michael.protsenko@gmail.com>
2016-03-05 15:20:17 -0500

    Converted Statistics to the module instead of class.

--- app/models/statistics.rb
+++ app/models/statistics.rb
@@ -1,77 +1,74 @@
-class Statistics
-  def adults_registered
+# Eithery Lab, 2016.
+# Statistics module.
+# Retrieves an event statistics info.
+
+module Statistics
+  def adults_total
     participants.count { |p| p.adult? }
   end
 
 
-  def adults_paid
+  def adults_paid_total
     participants.count { |p| p.adult? && p.paid? }
   end
 
 
-  def adults_onsite
+  def adults_onsite_total
     participants.count { |p| p.adult? && p.checked_in? }
   end
 
 
-  def children_registered
+  def children_total
     participants.count { |p| p.child? }
   end
 
 
-  def children_paid
+  def children_paid_total
     participants.count { |p| p.child? && p.paid? }
   end
 
 
-  def children_onsite
+  def children_onsite_total
     participants.count { |p| p.child? && p.checked_in? }
   end
 
 
-  def babies_registered
+  def babies_total
     participants.count { |p| p.baby? }
   end
 
 
-  def babies_paid
+  def babies_paid_total
     participants.count { |p| p.baby? && p.paid? }
   end
 
 
-  def babies_onsite
+  def babies_onsite_total
     participants.count { |p| p.baby? && p.checked_in? }
   end
 
 
-  def participants_registered
+  def participants_total
     participants.count
   end
 
 
-  def participants_paid
+  def participants_paid_total
     participants.count { |p| p.paid? }
   end
 
 
-  def participants_onsite
+  def participants_onsite_total
     participants.count { |p| p.checked_in? }
   end
 
 
-  def flagged
+  def flagged_total
     participants.count { |p| p.flagged? }
   end
 
 
-  def expected
+  def participants_expected_total
     participants.count { |p| p.paid? && !p.checked_in? }
-  end
-
-
-private
-
-  def participants
-    @participants ||= Participant.all.to_a
   end
 end
