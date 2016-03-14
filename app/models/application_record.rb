8fa5dc99df4458a491c6d74da84c817dfe0d9af7
Michael Protsenko <michael.protsenko@gmail.com>
2016-03-13 20:23:07 -0400

    Added some coments to ApplicationRecord model.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
+# Eithery Lab, 2016.
+# ApplcationRecord model.
+# Represents a base abstract class for all domain models.
+
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
