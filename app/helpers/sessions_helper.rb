fd289eeffd2831f11a07474477af38b56e68db4c
Michael Protsenko <michael.protsenko@gmail.com>
2016-03-15 16:24:14 -0400

    Refactored log_in, log_out, and current_user methods in SessionsHelper class.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,14 +5,27 @@
 module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
+  end
+
+
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
-    self.current_user = user
   end
 
 
   def log_out
-    self.current_user = nil
-    cookies.delete(:remember_token)
+    forget current_user
+    session.delete :user_id
+    @current_user = nil
+  end
+
+
+  def forget(user)
+    user.forget
+    cookies.delete :user_id
+    cookies.delete :remember_token
   end
 
 
@@ -21,13 +34,16 @@ module SessionsHelper
   end
 
 
-  def current_user=(user)
-    @current_user = user
-  end
-
-
   def current_user
-    @current_user #||= UserAccount.find_by(remember_token: cookies[:remember_token])
+    if(user_id = session[:user_id])
+      @current_user ||= UserAccount.find_by(id: user_id)
+    elsif(user_id = cookies.signed[:user_id])
+      user = UserAccount.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
 
