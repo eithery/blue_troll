d6fb10ee4d69d71d110cb2901101767efd9f9cc0
Michael Protsenko <michael.protsenko@gmail.com>
2016-03-17 23:09:16 -0400

    Created EventsController class and its specs file.

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,28 @@
+# Eithery Lab., 2016.
+# EventsController
+# Performs operations with private club events.
+
+class EventsController < ApplicationController
+  def index
+  end
+
+
+  def new
+  end
+
+
+  def create
+  end
+
+
+  def edit
+  end
+
+
+  def update
+  end
+
+
+  def destroy
+  end
+end
---
+++ spec/controllers/events_controller_spec.rb
@@ -0,0 +1,29 @@
+# Eithery Lab, 2016.
+# EventsController specs.
+
+require 'rails_helper'
+
+describe EventsController do
+  describe 'GET :index' do
+  end
+
+
+  describe 'GET :new' do
+  end
+
+
+  describe 'POST :create' do
+  end
+
+
+  describe 'GET :edit' do
+  end
+
+
+  describe 'PATCH :update' do
+  end
+
+
+  describe 'DELETE destroy' do
+  end
+end
