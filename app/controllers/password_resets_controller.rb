412e8750b60b0209bfa3c2475f0cda784d22c480
Michael Protsenko <michael.protsenko@gmail.com>
2016-03-17 22:55:50 -0400

    Created PasswordResetsController and its spec file.

---
+++ app/controllers/password_resets_controller.rb
@@ -0,0 +1,20 @@
+# Eithery Lab, 2016.
+# PasswordResetsController.
+# Performs forget password and password reset operations.
+
+class PasswordResetsController < ApplicationController
+  def new
+  end
+
+
+  def create
+  end
+
+
+  def edit
+  end
+
+
+  def update
+  end
+end
---
+++ spec/controllers/password_resets_controller_spec.rb
@@ -0,0 +1,21 @@
+# Eithery Lab, 2016.
+# PasswordResetsController specs.
+
+require 'rails_helper'
+
+describe PasswordResetsController do
+  describe 'GET :new' do
+  end
+
+
+  describe 'POST :create' do
+  end
+
+
+  describe 'GET :edit' do
+  end
+
+
+  describe 'PATCH :update' do
+  end
+end
