bd1110041f7ae2f6c7075d170066a352746e95a4
Michael Protsenko <michael.protsenko@gmail.com>
2016-03-18 09:46:11 -0400

    Created ShellController and its specs file.

---
+++ app/controllers/shell_controller.rb
@@ -0,0 +1,8 @@
+# Eithery Lab, 2016.
+# ShellController.
+# Performs control over landing and other static pages.
+
+class ShellController < ApplicationController
+  def landing
+  end
+end
---
+++ spec/controllers/shell_controller_spec.rb
@@ -0,0 +1,9 @@
+# Eithery Lab, 2016.
+# ShellController specs.
+
+require 'rails_helper'
+
+describe ShellController do
+  describe 'GET :landing' do
+  end
+end
