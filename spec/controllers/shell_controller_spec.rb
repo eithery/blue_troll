d4dc7b3d8170f525b5719a09f0ee4f1a2704da3d
Michael Protsenko <michael.protsenko@gmail.com>
2016-03-18 23:18:59 -0400

    Created specs for ShellController landing action.

--- spec/controllers/shell_controller_spec.rb
+++ spec/controllers/shell_controller_spec.rb
@@ -5,5 +5,11 @@ require 'rails_helper'
 
 describe ShellController do
   describe 'GET :landing' do
+    before { get :landing }
+
+    it { is_expected.to respond_with :ok }
+    it { is_expected.to respond_with_content_type :html }
+    it { is_expected.to render_with_layout :application }
+    it { is_expected.to render_template :landing }
   end
 end
