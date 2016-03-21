36fc4c39060601b3af27d3f149cf6f549e0d664d
Michael Protsenko <michael.protsenko@gmail.com>
2016-03-20 23:01:24 -0400

    Fixed KOA google map link.

--- app/helpers/landing_helper.rb
+++ app/helpers/landing_helper.rb
@@ -14,8 +14,6 @@ module LandingHelper
 
 
   def direction_to_koa
-    "https://www.google.com/maps/dir/''/koa+campground+delaware+water+gap/" +
-    "@41.0497396,-75.14573,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!" +
-    "1s0x89c4846ec4d7b2c3:0x614fa6c6947013e7!2m2!1d-75.0756901!2d41.0497608"
+    'https://goo.gl/maps/f6aDsBnDPTT2'
   end
 end
