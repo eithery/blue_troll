28245130c4c652fdee62bf91854c2bb033c3c7ff
Michael Protsenko <michael.protsenko@gmail.com>
2016-03-26 18:30:26 -0400

    Added size argument for gravatar_for helper method.

--- app/helpers/user_accounts_helper.rb
+++ app/helpers/user_accounts_helper.rb
@@ -3,9 +3,9 @@
 # Represents a helper class for user accounts views.
 
 module UserAccountsHelper
-  def gravatar_for(user_account)
+  def gravatar_for(user_account, size: 48)
     gravatar_id = Digest::MD5::hexdigest(user_account.email.downcase)
-    gravatar_url = "http://www.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "http://www.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user_account.name, class: 'img-circle')
   end
 
