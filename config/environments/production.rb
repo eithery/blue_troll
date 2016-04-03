6a9e6032b6c40c1bbd32631c827ad373022cb7a4
Michael Protsenko <michael.protsenko@gmail.com>
2016-04-03 19:51:29 -0400

    Set delivery error notifications.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "blue_troll_#{Rails.env}"
 
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'bluetrolley.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
