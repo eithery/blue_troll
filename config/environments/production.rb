647f5776a0b9f0a65fe0045851226d0c26c9365c
Michael Protsenko <michael.protsenko@gmail.com>
2016-04-03 19:44:07 -0400

    Configured SendGrid settings in production environment.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,9 +66,9 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'bluetrolley.herokuapp.com' }
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.sendgrid.com',
-    port: 587,
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
     authentication: 'plain',
     user_name: 'ENV[SENDGRID_USERNAME]',
     password: 'ENV[SENDGRID_PASSWORD]',
