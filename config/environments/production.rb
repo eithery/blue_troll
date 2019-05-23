cb0ddde3843562fb01da8607117544fdc22fa9b4
Michael Protsenko <michael.protsenko@gmail.com>
2019-05-22 20:22:13 -0400

    Configures SendGrid for emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,12 +67,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :mailhopper
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
-    address: ENV['MAILGUN_SMTP_SERVER'],
-    port: ENV['MAILGUN_SMTP_PORT'],
-    domain: 'mg.bluetrolley2019.herokuapp.com',
+    address: 'smtp.sendgrid.net',
+    port: 587,
     authentication: 'plain',
-    user_name: ENV['MAILGUN_SMTP_LOGIN'],
-    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
     enable_starttls_auto: true
   }
 
