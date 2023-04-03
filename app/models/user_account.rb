c47be51fbf318b527747920b21b05c039e692fe9
eithery <eithery.pro@gmail.com>
2023-04-02 23:51:44 -0400

    Temporary disable approve for crew leads

--- app/models/user_account.rb
+++ app/models/user_account.rb
@@ -60,7 +60,8 @@ class UserAccount < ActiveRecord::Base
 
 
   def can_approve?(participant)
-    !participant.approved? && (participant.crew_lead?(self) || self.admin?)
+#    !participant.approved? && (participant.crew_lead?(self) || self.admin?)
+    !participant.approved? && self.admin?
   end
 
 
