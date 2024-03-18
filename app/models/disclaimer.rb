a730dfce6d5792fdda64950f1d98c22933154458
eithery <eithery.pro@gmail.com>
2024-03-18 14:56:43 -0400

    Updated disclaimer

--- app/models/disclaimer.rb
+++ app/models/disclaimer.rb
@@ -31,16 +31,16 @@ class Disclaimer < Prawn::Document
       text "*#{@code}*", size: 10
     end
     bounding_box [300, cursor+60], width: 220 do
-      text @group_name, size: 20, align: :right
-      text @group_native_name, size: 16, align: :right
-      text @participant_name, size: 20, align: :right
-      text @age_category, size: 24, align: :right
+      text @group_name, size: 18, align: :right
+      text @group_native_name, size: 14, align: :right
+      text @participant_name, size: 16, align: :right
+      text @age_category, size: 18, align: :right
     end
 
-    move_down 4
+    move_down 0
     fill_color '17365D'
     indent 30 do
-      text 'Blue Trolley', size: 40
+      text 'Blue Trolley Inc.', size: 20
       stroke do
         stroke_color '1736A0'
         line_width 1
@@ -49,87 +49,105 @@ class Disclaimer < Prawn::Document
     end
 
 
-    move_down 15
-    fill_color '000000'
-    font 'calibri-bold'
-    text "Neversink River Resort, 108 Guymard Turnpike, Goddefroy, NY 12729\nFriday, June 7, 2024 - Sunday, June 9, 2024.",
-      size: 14, align: :center
-
     move_down 10
-    font 'arial', size: 12 do
+    fill_color '000000'
+    font 'calibri'
+    text 'Friday, June 7, 2024 - Sunday, June 9, 2024.', size: 12, align: :center
+
+    move_down 8
+    font 'arial', size: 10 do
       text 'INFORMED CONSENT / LIABILITY RELEASE', align: :center
     end
 
-    move_down 10
-    bounding_box([40, cursor], width: 450) do
-      font_size 11
+    move_down 8
+    bounding_box([25, cursor], width: 500) do
+      font_size 9
+      text 'I hereby make this waiver, disclaimer, release of liability, and indemnification agreement ' \
+        '(“Waiver and Release”), of my own volition in connection with the Event organized by Blue Trolley Inc. ' \
+        '(“Blue Trolley”).'
+      move_down 2
+      text "\u2022 I acknowledge that I have been fully and adequately advised by Blue Trolley of the risks and implications " \
+        'of using the Venue and participating in the Event, including inherent risks associated with waterborne activities. ' \
+        'I will not use the Venue and/or participate in the Event while intoxicated by alcohol, medications and/or illegal drugs.'
+      move_down 2
       text "\u2022 COVID-19 Warning. We have taken enhanced health and safety measures. " \
-        'An inherent risk of exposure to COVID-19 exists in any public place where people are present.'
-      move_down 3
-      text "\u2022 I understand and am fully aware of the fact that participation in the Event " \
-        'involves potential risk of injury.'
-      move_down 3
+        'However, an inherent risk of exposure to COVID-19 and other communicable diseases exists ' \
+        'in any public place where people are present.'
+      move_down 2
+      text "\u2022 I understand that participation in the Event involves potential risk of injury. " \
+        'Event Organizers are not responsible for accidents, negligent or intentional acts of third parties, personal injury, ' \
+        'serious injury and/or death, or damage to, destruction and/or loss of property. ' \
+        '“Event Organizers” shall mean Blue Trolley, all of its subsidiaries and/or affiliates, ' \
+        'and all of their directors, officers, employees, agents, and legal representatives. ' \
+        'I intentionally, knowingly and willfully accept all known and unknown risks associated with the Event ' \
+        'and my participating in the Event.'
+      move_down 2
       text "\u2022 I understand and acknowledge that all of the program activities in this Event are strictly voluntary. " \
-        'I will participate in these activities to whatever degree I deem appropriate of my own free choice ' \
-        'and only after due consideration of my own health, physical abilities and medical condition.'
-      move_down 3
-      text "\u2022 I agree that I am solely responsible for my own participation and emotional well-being."
-      move_down 3
-      text "\u2022 I agree that I am fully responsible for well-being of all persons, whom I am the legal guardian " \
-        'for during the Event.'
-      move_down 3
-      text "\u2022 I willingly and knowingly assume for myself and all persons whom I am responsible for, " \
-        'all risk of physical injury and emotional upset, which may occur during or after participating ' \
-        'in any aspect of the Event.'
-      move_down 3
-      text "\u2022 I agree to hold the Venue and the Event organizers harmless of any liability arising " \
-        'out of my participation in the Event.'
-      move_down 3
-      text "\u2022 Should the Venue, Event organizers or any party acting on their behalf be required to " \
-        'incur attorney’s fees, or any other costs to enforce this agreement, I agree to indemnity and hold the Venue, ' \
-        'the Event organizers or any party acting on their behalf, harmless for all such fees and costs.'
+        'I will participate in these activities to whatever degree I deem appropriate and only after due consideration of my own ' \
+        'health, physical abilities and medical condition.'
+      move_down 2
+      text "\u2022 I agree that I am voluntarily participating in the Event and I am fully responsible for my physical " \
+        'and emotional well-being. I also agree that I am fully responsible for well-being of all persons of whom I am the legal ' \
+        'guardian during the Event, including without limitation minor children, special needs individuals, or disabled ' \
+        'individuals, and I acknowledge that I am responsible for their continuous supervision during the Event.'
+      move_down 2
+      text "\u2022 I willingly and knowingly assume for myself and all persons for whom I am responsible, all risk of " \
+        'physical injury, death and emotional distress, which may occur during or after participating in any aspect of the Event.'
+      move_down 2
+      text "\u2022 I agree to indemnify, defend, and hold harmless the Event Organizers and Venue from any and all claims, " \
+        'demands, actions, expenses, attorney’s fees, and liability whatsoever, whether suspected or unsuspected, vested or ' \
+        'contingent, in law or in equity, existing by statute, common law, contract, or otherwise which have existed, do exist ' \
+        'or may exist, in any way relating to any and all losses, injuries and damages of any and every kind, to person or ' \
+        'property, arising out of my participation in the Event and/or the participation of individuals of whom I am the legal guardian.'
+      move_down 2
+      text "\u2022 If the Event Organizers or any party acting on their behalf be required to incur attorney’s fees, or any other " \
+        'costs, to enforce this Waiver and Release, I agree to indemnity and hold them harmless for all such fees and costs.'
+      move_down 2
+      text "\u2022 For and in consideration of the services provided in connection with the Event, the Event Organizers and " \
+        'Venue shall not be held responsible and/or liable, and are hereby released from responsibility and/or liability for any ' \
+        'loss, loss of property, damage, injury, accident and/or death associated with the Event.'
+      move_down 2
+      text "\u2022 It is understood and agreed that this Waiver and Release contains the complete agreement of the parties " \
+        'concerning the subject matter, and supersedes any prior oral or written understandings, representations, or ' \
+        'agreements pertaining thereto. This Waiver and Release and its terms are contractual and not merely a recital and ' \
+        'shall be construed and governed by the laws of the State of New York, without regard to its conflicts of laws’ provisions.'
+      move_down 2
+      text 'I have read and understand this Waiver and Release, acknowledge the risks and liability for myself and on ' \
+        'behalf of all persons of whom I am the legal guardian, and freely and voluntarily agree to its terms and conditions, ' \
+        'without inducement, promise or guarantee. I am aware that by signing this document, I am knowingly and willfully ' \
+        'giving up important rights, and I represent that I am the person identified below who is signing this document, ' \
+        'and/or that I am the lawful parent or legal guardian of such person and have the authority to sign on his/her behalf. ' \
+        'This Waiver and Release shall be binding upon and inure to the benefit of the parties, their heirs, successors, assigns' \
+        'and legal representatives.'
     end
 
-    move_down 7
-    bounding_box([40, cursor], width: 460) do
-      text "On this day of ____________________, 2024, I, ___________________________________________ ,\n" \
+    move_down 5
+    bounding_box([25, cursor], width: 520) do
+      text "On this day of ____________________, 2024, I, ___________________________________________ , " \
         'of my own free will, have read, understand and acknowledge the risks and liability for myself ' \
         'and on behalf of all persons for who I am the legal guardian during the Event.'
     end
 
-    move_down 25
+    move_down 18
     stroke do
       stroke_color '000000'
-      horizontal_line 40, 240, at: cursor
+      horizontal_line 25, 240, at: cursor
     end
     font_size 9
-    move_down 3
-    indent 40 do
+    move_down 2
+    indent 25 do
       text 'Participant Signature'
     end
 
-    move_down 25
+    move_down 12
     stroke do
-      horizontal_line 40, 240, at: cursor
+      horizontal_line 25, 240, at: cursor
       horizontal_line 290, 490, at: cursor
     end
 
-    move_down 3
-    indent 40 do
-      text 'Parent/Guardian Signature' + spaces(75) + 'Printed Parent/Guardian Name'
-    end
-
-    move_down @address.blank? ? 25 : 15
-    indent 40 do
-      text @address, size: 12
-    end
-
-    stroke do
-      horizontal_line 40, 490, at: cursor
-    end
-    move_down 3
-    indent 40 do
-      text 'Address' + spaces(80) + 'City' + spaces(30) + 'State' + spaces(30) + 'Zip'
+    move_down 2
+    indent 25 do
+      text 'Parent/Guardian Signature' + spaces(90) + 'Printed Parent/Guardian Name'
     end
     self
   end
